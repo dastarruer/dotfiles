676559f9239568b2a79c353e76ed7f961dc36be5
dastarruer <ayushpramanik399@gmail.com>
2024-10-29 22:31:57 -0400

    Automated commit

---
+++ Code/User/History/-594d8ef/gEY5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# The .config directory
+CONFIG="$HOME/.config"
+
+for dir in "$CONFIG"/*/; do 
+    echo "Adding $dir"
+    cd "$dir" || continue
+
+    git add .
+
+    echo "Changes for $dir pushed"
+    done
+git commit -m "Automated commit"
+git push 
+
---
+++ Code/User/History/-594d8ef/va8M.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# The .config directory
+CONFIG="$HOME/.config"
+
+for dir in "$CONFIG"/*/; do 
+    echo "Adding $dir"
+    cd "$dir" || continue
+
+    git add .
+    git commit -m "Automated commit"
+
+    echo "Changes for $dir pushed"
+    done
+git push 
+
