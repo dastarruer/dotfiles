421651f2b8fab1e31188ee59d8a47c14feca9c6b
dastarruer <ayushpramanik399@gmail.com>
2025-03-06 19:47:17 -0400

    add script to connect headphones

--- bin/bin/airpods.sh
+++ bin/bin/airpods.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
-# AirPods MAC Address 
-source ~/.airpods_mac
+# Headphones MAC Address 
+source ~/.headphones_mac
 
-notify-send -t 4000 "Restarting bluetooth..."
-bluetoothctl <<EOF
-power off
-exit
-EOF
-sleep 5  # Give Bluetooth time to restart
+dunstctl close-all
+bluetoothctl power on
+if bluetoothctl info "$HEADPHONES_MAC" | grep 'Connected: yes' -q; then
+    # Restart bluetooth
+    notify-send -t 4000 "Restarting bluetooth..."
+    bluetoothctl power off
+    bluetoothctl power on
 
-notify-send -t 1000 "Connecting to AirPods..."
-bluetoothctl <<EOF
-power on
-agent on
-default-agent
-trust $AIRPODS_MAC
-connect $AIRPODS_MAC
-EOF
-
-# Check if connected
-if bluetoothctl info "$AIRPODS_MAC" | grep -q "Connected: yes"; then
-    notify-send -t 1000 "✅ Successfully connected to AirPods!"
+    # Connect headphones
+    notify-send -t 4000 "Connecting to headphones..."
+    bluetoothctl connect "$HEADPHONES_MAC"
 else
-    notify-send -t 1000 "❌ Failed to connect. Try again."
+    # Connect headphones
+    notify-send -t 4000 "Connecting to headphones..."
+    bluetoothctl connect "$HEADPHONES_MAC"
 fi
+
+# notify-send -t 4000 "Restarting bluetooth..."
+# bluetoothctl <<EOF
+# power off
+# exit
+# EOF
+
+# notify-send -t 1000 "Connecting to AirPods..."
+# bluetoothctl <<EOF
+# power on
+# agent on
+# default-agent
+# trust $HEADPHONES_MAC
+# connect $HEADPHONES_MAC
+# EOF
+# notify-send -t 4000 "Done!"
+
+# # Check if connected
+# if bluetoothctl info "$HEADPHONES_MAC" | grep -q "Connected: yes"; then
+#     notify-send -t 1000 "✅ Successfully connected to AirPods!"
+# else
+#     notify-send -t 1000 "❌ Failed to connect. Try again."
+# fi
