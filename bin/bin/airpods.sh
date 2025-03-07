728fb6b7d0fb8513a6c34ab524817191a4fb3d00
dastarruer <ayushpramanik399@gmail.com>
2025-03-06 21:42:06 -0400

    airpods script

--- bin/bin/airpods.sh
+++ bin/bin/airpods.sh
@@ -20,25 +20,3 @@ else
     bluetoothctl connect "$HEADPHONES_MAC"
 fi
 
-# notify-send -t 4000 "Restarting bluetooth..."
-# bluetoothctl <<EOF
-# power off
-# exit
-# EOF
-
-# notify-send -t 1000 "Connecting to AirPods..."
-# bluetoothctl <<EOF
-# power on
-# agent on
-# default-agent
-# trust $HEADPHONES_MAC
-# connect $HEADPHONES_MAC
-# EOF
-# notify-send -t 4000 "Done!"
-
-# # Check if connected
-# if bluetoothctl info "$HEADPHONES_MAC" | grep -q "Connected: yes"; then
-#     notify-send -t 1000 "✅ Successfully connected to AirPods!"
-# else
-#     notify-send -t 1000 "❌ Failed to connect. Try again."
-# fi
