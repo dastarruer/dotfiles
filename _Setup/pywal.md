b18ca93d08297f0493e3b098d67d3d7b0e5328e5
dastarruer <ayushpramanik399@gmail.com>
2025-03-09 19:50:34 -0400

    spicetify pywal theme

---
+++ _Setup/pywal.md
@@ -0,0 +1,4 @@
+The following need to be symlinked for pywal to work:
+- betterlockscreenrc
+- color.ini for spicetify
+- dunstrc
--- bin/bin/airpods.sh
+++ bin/bin/airpods.sh
@@ -14,7 +14,7 @@ if bluetoothctl info "$HEADPHONES_MAC" | grep 'Connected: yes' -q; then
 
     # Connect headphones
     notify-send -t 4000 "Connecting to headphones..."
-    bluetoothctl connect "$HEADPHONES_MAC"
+    bluetoothctl connect "$HEADPHONES_MAC" || notify-send -t "Failed to connect headphones. Please try again."
     playerctl play 
 else
     # Connect headphones
