cbe42739edc29d8fe473ea8e2a70426c8897fda7
dastarruer <ayushpramanik399@gmail.com>
2025-03-10 18:38:17 -0400

    change ffmpeg convert script for legally obtained media

--- bin/bin/convert_media.sh
+++ bin/bin/convert_media.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
-# Use fzf to choose input directory
-input_dir=/media/to_convert
+if [ -z "$1" ]; then
+  echo "Show name is missing!"
+  exit 1
+fi
 
-# Use fzf to choose output directory
-output_dir=$1
+# Hardcode input and output directories
+input_dir="~/Videos/to_convert/"
+output_dir="/media/jellyfin/shows/$1"
 
-# Loop through all video files in the selected input directory
+# Check if input directory exists
+if [ ! -d "$input_dir" ]; then
+  echo "Input directory does not exist. Exiting."
+  exit 1
+fi
+
+# Check if output directory exists
+if [ ! -d "$output_dir" ]; then
+  echo "Output directory does not exist. Exiting."
+  exit 1
+fi
+
+# Loop through all video files in the input directory
 for input_file in "$input_dir"/*; do
   if [[ "$input_file" == *.mkv || "$input_file" == *.avi || "$input_file" == *.mov ]]; then
+    # Set the output file path
     output_file="$output_dir/$(basename "$input_file" | sed 's/\.[^.]*$/.mp4/')"
-    echo "Converting $input_file to $output_file"
     
-    ffmpeg -v warning -i "$input_file" -c:v libx264 -c:a aac -strict experimental "$output_file" > /var/log/convert_media$(date +'%Y-%m-%d_%H-%M-%S').log 2>&1
+    # Set log file path
+    log_file="$output_dir/$(basename "$input_file" | sed 's/\.[^.]*$/.log/')"
+    
+    # Convert the file with ffmpeg and log the output
+    ffmpeg -hide_banner -v error -i "$input_file" -c:v libx264 -c:a aac "$output_file" > "$log_file" 2>&1
   fi
 done
