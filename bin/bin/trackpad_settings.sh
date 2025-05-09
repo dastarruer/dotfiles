1746dcd96f097702d89dfa3184f8ae753fbe82fa
dastarruer <ayushpramanik399@gmail.com>
2025-05-09 08:25:05 -0400

    stuff

--- bin/bin/trackpad_settings.sh
+++ bin/bin/trackpad_settings.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xinput set-prop 17 "libinput Tapping Enabled" 1
-xinput set-prop 17 "libinput Natural Scrolling Enabled" 1
+xinput set-prop 11 "libinput Tapping Enabled" 1
+xinput set-prop 11 "libinput Natural Scrolling Enabled" 1
