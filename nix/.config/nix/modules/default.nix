017b07e236dcdc083d8b48984fcde8c75dc33ee8
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-06-15 19:31:35 -0400

    stuff

--- nix/.config/nix/modules/default.nix
+++ nix/.config/nix/modules/default.nix
@@ -3,7 +3,6 @@
     ./users.nix
     ./locale.nix
     ./bootloader.nix
-    ./audio.nix
     ./hyprland.nix
     ./packages.nix
     ./keyboard.nix
--- nix/.config/nix/modules/hyprland.nix
+++ nix/.config/nix/modules/hyprland.nix
@@ -1,6 +1,22 @@
-{...}: {
+{pkgs, ...}: {
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
   };
+
+  # Enable sound with pipewire, which is better for wayland
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Hyprland packages needed
+  environment.systemPackages = with pkgs; [
+    waybar
+    dunst
+  ];
 }
