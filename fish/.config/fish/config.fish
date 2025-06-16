dd33b502b556d60f0be31091ab549040c02445e3
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-06-16 17:46:01 -0400

    stuff

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -26,7 +26,6 @@ if status --is-interactive
     alias backup="backup.sh"
     alias sl="ls"
     alias zth="zathura"
-    # Git Aliases
     alias ga="git add"
     alias gb="git branch"
     alias g.="git add . && git status"
@@ -36,56 +35,19 @@ if status --is-interactive
     alias gp="git push & disown"
     alias gl="git log --oneline --graph --decorate"
     alias gch="git checkout"
-    
     alias cr="cargo run"
     alias ct="cargo test"
     alias y="yazi"
     alias ai="ollama run llama3.2"    
     alias wlist="nmcli d wifi list"
     alias wconnect="nmcli d wifi connect"
-
     alias convert="convert_wallpaper.sh"
-	
-    # Fzf
     alias fdf="fzfdf.sh"
     alias remove_wallpaper="remove_wallpaper.sh"
-
     alias picom-edit="nano ~/.dotfiles/picom/.config/picom/picom.conf"
     alias i3-edit="nano ~/.dotfiles/i3/.config/i3/config"
     alias polybar-edit="code ~/.dotfiles/polybar/.config/polybar/."
     alias fish-edit="nano ~/.dotfiles/fish/.config/fish/config.fish"
-
-    # Games
-    # To create a steam game function:
-        # This is the proton prefix that the game will be played in (note that the directory specified has to be created manually)
-            # env STEAM_COMPAT_DATA_PATH=$HOME/.local/share/Steam/steamapps/compatdata/{game_name} \
-        # This is where steam is installed
-            # STEAM_COMPAT_CLIENT_INSTALL_PATH=$HOME/.steam/debian-installation \
-        # Specify the path to proton, and run
-            # $HOME/.steam/steam/steamapps/common/Proton\ 9.0\ \(Beta\)/proton run \
-            # path/to/executable
-    function superhot
-        env STEAM_COMPAT_DATA_PATH=$HOME/.local/share/Steam/steamapps/compatdata/superhot \
-        STEAM_COMPAT_CLIENT_INSTALL_PATH=$HOME/.steam/debian-installation \
-        $HOME/.steam/steam/steamapps/common/Proton\ 9.0\ \(Beta\)/proton run \
-        $HOME/Games/SUPERHOT/SH.exe
-    end 
-
-    function hitman
-        env STEAM_COMPAT_DATA_PATH=$HOME/.local/share/Steam/steamapps/compatdata/hitman \
-        STEAM_COMPAT_CLIENT_INSTALL_PATH=$HOME/.steam/debian-installation \
-        $HOME/.steam/steam/steamapps/common/Proton\ 9.0\ \(Beta\)/proton run \
-        $HOME/Games/HITMAN\ World\ of\ Assassination/Launcher.exe
-    end
-
-    function nightrunners
-        env STEAM_COMPAT_DATA_PATH=$HOME/.local/share/Steam/steamapps/compatdata/nightrunners \
-        STEAM_COMPAT_CLIENT_INSTALL_PATH=$HOME/.steam/debian-installation \
-        $HOME/.steam/steam/steamapps/common/Proton\ 9.0\ \(Beta\)/proton run \
-        $HOME/.steam/debian-installation/steamapps/common/NIGHT-RUNNERS\ PROLOGUE/NIGHT-RUNNERS\ PROLOGUE.exe
-    end
-
-    # Safer aliases for cp and rm
     alias cp="cp -r"
     alias rm="rm -r"
 end
--- rofi/.config/rofi/scripts/switch_theme.sh
+++ rofi/.config/rofi/scripts/switch_theme.sh
@@ -42,10 +42,10 @@ esac
 notify-send -t 300 "Updating pywal..."
 wal --theme "$WAL"
 
-ln -s $HOME/.cache/wal/colors-zathura $HOME/.config/zathura/zathurarc
-ln -s $HOME/.cache/wal/betterlockscreenrc $HOME/.config/betterlockscreen/betterlockscreenrc
-ln -s $HOME/.cache/wal/colors-spicetify.ini $HOME/.config/spicetify/Themes/Onepunch/color.ini
-ln -s $HOME/.cache/wal/dunstrc $HOME/.config/dunstrc
+# Symlink a bunch of files
+ln -sf $HOME/.cache/wal/zathurarc $HOME/.config/zathura/zathurarc
+ln -sf $HOME/.cache/wal/spicetify.ini $HOME/.config/spicetify/Themes/Onepunch/color.ini
+ln -sf $HOME/.cache/wal/dunstrc $HOME/.config/dunstrc
 
 # Restart Dunst to apply changes
 DUNST_CONFIG="$HOME/.cache/wal/dunstrc"
