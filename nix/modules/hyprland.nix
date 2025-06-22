ad55911509d6ab969e8c6c97b0d9e75f570e23df
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-06-22 15:11:44 -0400

    add hyprlock

--- nix/modules/hyprland.nix
+++ nix/modules/hyprland.nix
@@ -9,6 +9,9 @@
     withUWSM = true;
   };
 
+  # Lockscreen
+  programs.hyprlock.enable = true;
+
   # For stuff between apps like clipboard access, drag and drop, etc.
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
