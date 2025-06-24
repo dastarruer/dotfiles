12164b3218726dadc04cc6d2ecbe9b535f776e0d
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-06-23 22:04:15 -0400

    add something to bluetooth ig

--- nix/modules/bluetooth.nix
+++ nix/modules/bluetooth.nix
@@ -1,6 +1,19 @@
 {...}: {
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  hardware.bluetooth = {
+    # Enable support for Bluetooth
+    enable = true;
+
+    # powers up the default Bluetooth controller on boot
+    powerOnBoot = true;
+
+    # Set bluetooth settings
+    settings = {
+      # Because it says I should do this for airpods here: https://gist.github.com/aidos-dev/b49078c1d8c6bb1621e4ac199d18213b
+      General = {
+        ControllerMode = "bredr";
+      };
+    };
+  };
 
   # blueman-manager
   services.blueman.enable = true;
