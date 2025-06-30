fcaecaf09665799618ddbc82d0d6cd10b32788d7
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-06-29 23:05:49 -0400

    stuff

--- nix/modules/system-packages.nix
+++ nix/modules/system-packages.nix
@@ -6,6 +6,9 @@
   # List packages installed in system profile
   environment.systemPackages = with pkgs; [
     libnotify
+
+    # Git push fails if this is not a system package instead of a user one
+    gh
   ];
 
   # For nixd i think
