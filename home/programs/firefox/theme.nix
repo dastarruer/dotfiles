9c6d20f6ca1db9a52c62768220e3915f358f4e1c
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-11 17:05:18 -0400

    stuff

--- home/programs/firefox/theme.nix
+++ home/programs/firefox/theme.nix
@@ -1,12 +1,12 @@
 {firefoxProfile, ...}: let
   # Firefox theme that I will use
-  firefox-theme = "https://github.com/cascadefox/cascade";
+  firefoxTheme = "https://github.com/cascadefox/cascade";
 in {
   # Fetch firefox theme (https://www.reddit.com/r/NixOS/comments/1f5wbjd/installing_a_complex_user_css_for_firefox/)
   home.file = {
     ".mozilla/firefox/${firefoxProfile}/chrome" = {
       source = "${builtins.fetchGit {
-        url = firefox-theme;
+        url = firefoxTheme;
         rev = "f8c6bb5a36f24aba61995204ff5497c865e78e50";
       }}/chrome";
       recursive = true;
