b39639c69578119063875964b7c5c84fd9f48276
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-13 22:33:58 -0400

    even more nix settings

--- home/programs/vscodium/settings/appearance.nix
+++ home/programs/vscodium/settings/appearance.nix
@@ -3,8 +3,15 @@
   programs.vscode.profiles.default.userSettings = {
     # UI
     "workbench.sideBar.location" = "right";
-    "workbench.activityBar.location" = "top";
+    "workbench.activityBar.location" = "hidden";
     "workbench.statusBar.visible" = false;
+    "workbench.editor.showTabs" = "single";
+    "workbench.startupEditor" = "none";
+    "workbench.tips.enabled" = false;
+    "breadcrumbs.enable" = false;
+    "workbench.tree.renderIndentGuides" = false;
+    "workbench.tree.indent" = 16;
+    "explorer.decorations.badges" = false;
     "editor.minimap.enabled" = false;
 
     # Appearance
