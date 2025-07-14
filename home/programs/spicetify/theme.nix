884bae7fd7b44d3897147cb3bb4a1aab5ce3a162
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-13 21:56:59 -0400

    update system

--- home/programs/spicetify/theme.nix
+++ home/programs/spicetify/theme.nix
@@ -7,7 +7,7 @@
       owner = "Skaytacium";
       repo = "Gruvify";
       rev = "main";
-      sha256 = "sha256-9KBA7ettPHjh7CH1QSuwbeLJCh2cdHXFghZeywVtRlI=";
+      sha256 = "sha256-9kjoOd59c6/AUKQd0m+HVacrySZ+2JSGXK7Rb5IHH3o=";
     };
   };
 }
