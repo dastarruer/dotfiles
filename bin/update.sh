3e55ac3e69762661e85e2ff14afd0327fe581b11
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-15 12:56:58 -0400

    stuff

--- bin/update.sh
+++ bin/update.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-sudo apt update
-sudo apt upgrade -y
-sudo apt autoremove -y
-flatpak update -y
-pipx upgrade-all
-spicetify update
-spicetify restore backup apply
+cd $HOME/.dotfiles
+nix flake update
+$HOME/bin/commit_dotfiles.sh "Update system"
+sudo nixos-rebuild switch --flake ~/.dotfiles/
