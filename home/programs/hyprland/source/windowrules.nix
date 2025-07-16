a2702681a148418d4354cbb1c7dbce263aa98351
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-15 21:12:19 -0400

    stuff

--- home/programs/hyprland/source/windowrules.nix
+++ home/programs/hyprland/source/windowrules.nix
@@ -4,7 +4,6 @@
       "opacity 1.0 override, class:^(firefox|thunar)$"
       "suppressevent maximize, class:.*"
       "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
-      ""
     ];
 
     windowrulev2 = [
