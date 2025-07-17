bbe7712040faeeee7795b4f4cd6318e52c6e36fd
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-17 10:53:05 -0400

    remove hyprland plugins to reduce build times

--- flake.nix
+++ flake.nix
@@ -31,10 +31,10 @@
 
     hyprland.url = "github:hyprwm/Hyprland";
 
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-    };
+    # hyprland-plugins = {
+    #   url = "github:hyprwm/hyprland-plugins";
+    #   inputs.hyprland.follows = "hyprland";
+    # };
 
     # Microcode updates
     cpu-microcodes = {
