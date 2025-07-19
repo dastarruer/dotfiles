08a33bae491fb26c0d67c9184b003393ffd3f027
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-18 21:59:23 -0400

    stuff

--- home/programs/hyprland/hypridle.nix
+++ home/programs/hyprland/hypridle.nix
@@ -3,15 +3,22 @@
     enable = true;
 
     settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
+        before_sleep_cmd = "hyprlock"; # lock before suspend.
+      };
+
       listener = [
         {
-          timeout = 600;
-          on-timeout = "hyprlock && systemctl suspend";
+          # Turn off screen
+          timeout = 300;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
         }
         {
-          timeout = 300;
-          on-timeout = "hyprlock && hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
+          # Suspend system
+          timeout = 600;
+          on-timeout = "systemctl suspend";
         }
       ];
     };
