7c36464628d06dbf6e3afba645c096ee96613517
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-19 19:02:53 -0400

    add cursor theme

--- home/gtk.nix
+++ home/gtk.nix
@@ -3,12 +3,19 @@
   lib,
   ...
 }: let
+  # Font-related settings
   fontPackage = pkgs.noto-fonts;
   fontName = "Noto Sans 12";
+
+  # Cursor-related settings
+  cursorPackage = pkgs.capitaine-cursors-themed;
+  cursorName = "Capitaine Cursors (Gruvbox)";
+  cursorSize = 24;
 in {
   gtk = {
     enable = true;
 
+    # Font
     font = {
       package = fontPackage;
       name = fontName;
@@ -16,6 +23,18 @@ in {
     gtk2.font = {
       package = fontPackage;
       name = fontName;
+    };
+
+    # Cursors
+    cursorTheme = {
+      package = cursorPackage;
+      name = cursorName;
+      size = cursorSize;
+    };
+    gtk2.cursorTheme = {
+      package = cursorPackage;
+      name = cursorName;
+      size = cursorSize;
     };
   };
 
