d95ec29a179b68cb224289572d1b830fa2b50d46
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-18 21:23:30 -0400

    add even more settings

--- nix/modules/misc/power.nix
+++ nix/modules/misc/power.nix
@@ -4,10 +4,16 @@
 
   services.logind = {
     # Suspend system when power button is pressed
+    powerKey = "suspend";
     suspendKey = "suspend";
+    rebootKey = "suspend";
+    hibernateKey = "suspend";
 
     # Reboot system on long press
+    powerKeyLongPress = "reboot";
     suspendKeyLongPress = "reboot";
+    rebootKeyLongPress = "reboot";
+    hibernateKeyLongPress = "reboot";
 
     # Auto-suspend after 10 minutes
     extraConfig = ''
