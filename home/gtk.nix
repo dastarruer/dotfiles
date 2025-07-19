c84cd62395acf4e1944d472a8be146bb9d86aa4f
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-19 18:59:12 -0400

    fix setting conflicts

--- home/gtk.nix
+++ home/gtk.nix
@@ -1,10 +1,25 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  fontPackage = pkgs.noto-fonts;
+  fontName = "Noto Sans 12";
+in {
   gtk = {
     enable = true;
 
     font = {
-      package = pkgs.noto-fonts;
-      name = "Noto Sans";
+      package = fontPackage;
+      name = fontName;
+    };
+    gtk2.font = {
+      package = fontPackage;
+      name = fontName;
     };
   };
+
+  # chatgpt told me to use this to prevent conflicts and it works so
+  dconf.settings."org/gnome/desktop/interface".font-name =
+    lib.mkForce fontName;
 }
