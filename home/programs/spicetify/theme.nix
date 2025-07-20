0c50b6009d7e28fd1a94aa369041e6ec10441988
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-19 22:38:45 -0400

    use onepunch theme instead

--- home/programs/spicetify/theme.nix
+++ home/programs/spicetify/theme.nix
@@ -1,13 +1,22 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   # Spicetify themes
-  programs.spicetify.theme = {
-    name = "Gruvify";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "Skaytacium";
-      repo = "Gruvify";
-      rev = "main";
-      sha256 = "sha256-9kjoOd59c6/AUKQd0m+HVacrySZ+2JSGXK7Rb5IHH3o=";
-    };
+  programs.spicetify = {
+    theme = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system}.themes.onepunch;
+    colorScheme = "dark";
   };
 }
+# UNUSED
+# programs.spicetify.theme = {
+#   name = "Gruvify";
+#   src = pkgs.fetchFromGitHub {
+#     owner = "Skaytacium";
+#     repo = "Gruvify";
+#     rev = "main";
+#     sha256 = "sha256-9kjoOd59c6/AUKQd0m+HVacrySZ+2JSGXK7Rb5IHH3o=";
+#   };
+# };
+
