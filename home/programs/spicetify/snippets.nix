9dabeffc031f852699641e773c36896b9d8ad909
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-20 11:38:44 -0400

    organize snippets.nix

--- home/programs/spicetify/snippets.nix
+++ home/programs/spicetify/snippets.nix
@@ -7,18 +7,29 @@
 in {
   # CSS snippets to make spotify look even better
   programs.spicetify.enabledSnippets = with spicePkgs.snippets; [
+    # Disable some things
     disableRecommendations
+
+    # Remove some things
     removeGradient
-    disableRecommendations
-    pointer
-    modernScrollbar
+    removeTopSpacing
+
+    # Hide some things
     hideDownloadButton
     hideFriendActivityButton
     hideFullScreenButton
-    roundedButtons
-    removeTopSpacing
-    hideFullScreenButton
     hideMiniPlayerButton
+
+    # Round some things
+    roundedButtons
     roundedImages
+
+    # Fix some things
+    fixNowPlayingIcon
+    fixLikedButton
+
+    # Other
+    pointer
+    modernScrollbar
   ];
 }
