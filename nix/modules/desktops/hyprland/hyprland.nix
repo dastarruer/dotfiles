1772e23d8b753204818411d94001cff857978ae0
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-19 21:27:51 -0400

    move stuff to hm

--- nix/modules/desktops/hyprland/hyprland.nix
+++ nix/modules/desktops/hyprland/hyprland.nix
@@ -8,11 +8,4 @@
     # Start hyprland on boot
     withUWSM = true;
   };
-
-  # Lockscreen
-  programs.hyprlock.enable = true;
-
-  # For stuff between apps like clipboard access, drag and drop, etc.
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
 }
