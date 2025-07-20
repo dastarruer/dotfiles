5a2b9abcde974c7be3598d35bf4ffa81b81f886b
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-19 23:19:19 -0400

    extend custom color scheme

--- home/programs/spicetify/theme.nix
+++ home/programs/spicetify/theme.nix
@@ -2,11 +2,30 @@
   pkgs,
   inputs,
   config,
+  lib,
   ...
 }: {
   # Spicetify themes
   programs.spicetify = {
-    theme = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system}.themes.onepunch;
+    theme = {
+      name = "Gruvify";
+      src = pkgs.fetchFromGitHub {
+        owner = "riasat-sheikh";
+        repo = "Onepunch";
+        rev = "main";
+        sha256 = "sha256-nGxMc9mtshWsCQS8B/dPNXXlWeFM58HvWmp7Aj2/V2c=";
+      };
+
+      additionalCss = lib.concatStringsSep "\n" [
+        ".main-rootview-container; .main-navBar-navBar; .main-topBar-container; .Root__nav-bar; .Root__top-bar {"
+        "  background-color =  ${config.lib.stylix.colors.base00};"
+        "}"
+
+        ".main-view-container__scroll-node-child .main-entityHeader-container {"
+        "  background-color =  ${config.lib.stylix.colors.base00};"
+        "}"
+      ];
+    };
 
     customColorScheme = {
       accent = "${config.lib.stylix.colors.base00}";
@@ -22,6 +41,20 @@
       notificationError = "${config.lib.stylix.colors.base08}";
       subtext = "${config.lib.stylix.colors.base05}";
       text = "${config.lib.stylix.colors.base05}";
+      extratext = "${config.lib.stylix.colors.base05}";
+      sidebar = "${config.lib.stylix.colors.base00}";
+      player = "${config.lib.stylix.colors.base04}";
+      secPlayer = "${config.lib.stylix.colors.base04}";
+      card = "${config.lib.stylix.colors.base02}";
+      secCard = "${config.lib.stylix.colors.base03}";
+      shadow = "${config.lib.stylix.colors.base01}";
+      selectedRow = "${config.lib.stylix.colors.base02}";
+      button = "${config.lib.stylix.colors.base04}";
+      buttonActive = "${config.lib.stylix.colors.base05}";
+      buttonDisabled = "${config.lib.stylix.colors.base01}";
+      tabActive = "${config.lib.stylix.colors.base06}";
+      misc = "${config.lib.stylix.colors.base03}";
+      defaultFgColor = "${config.lib.stylix.colors.base05}";
     };
   };
 }
