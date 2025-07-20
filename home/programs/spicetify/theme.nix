e8d3d5f804eab640dff3d1a9b7f6fe68354dcf11
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-20 12:02:31 -0400

    add color to spicetify

--- home/programs/spicetify/theme.nix
+++ home/programs/spicetify/theme.nix
@@ -9,20 +9,18 @@
     theme = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system}.themes.onepunch;
 
     customColorScheme = {
-      accent = "${config.lib.stylix.colors.base00}";
-      accentActive = "${config.lib.stylix.colors.base02}";
+      # More vivid accents & UI highlights
+      accent = "${config.lib.stylix.colors.base0C}";
+      accentActive = "${config.lib.stylix.colors.base0D}";
       accentInactive = "${config.lib.stylix.colors.base03}";
-      banner = "${config.lib.stylix.colors.base04}";
-      borderActive = "${config.lib.stylix.colors.base05}";
-      borderInactive = "${config.lib.stylix.colors.base05}";
-      header = "${config.lib.stylix.colors.base05}";
-      highlight = "${config.lib.stylix.colors.base06}";
+      banner = "${config.lib.stylix.colors.base0A}";
+      borderActive = "${config.lib.stylix.colors.base08}";
+      borderInactive = "${config.lib.stylix.colors.base01}";
+      header = "${config.lib.stylix.colors.base0E}";
+      highlight = "${config.lib.stylix.colors.base0B}";
+
+      # Background-based elements (restored to original muted tone)
       main = "${config.lib.stylix.colors.base00}";
-      notification = "${config.lib.stylix.colors.base07}";
-      notificationError = "${config.lib.stylix.colors.base08}";
-      subtext = "${config.lib.stylix.colors.base05}";
-      text = "${config.lib.stylix.colors.base05}";
-      extratext = "${config.lib.stylix.colors.base05}";
       sidebar = "${config.lib.stylix.colors.base00}";
       player = "${config.lib.stylix.colors.base00}";
       secPlayer = "${config.lib.stylix.colors.base04}";
@@ -30,12 +28,25 @@
       secCard = "${config.lib.stylix.colors.base03}";
       shadow = "${config.lib.stylix.colors.base01}";
       selectedRow = "${config.lib.stylix.colors.base02}";
-      button = "${config.lib.stylix.colors.base04}";
-      buttonActive = "${config.lib.stylix.colors.base05}";
-      buttonDisabled = "${config.lib.stylix.colors.base01}";
-      tabActive = "${config.lib.stylix.colors.base06}";
-      misc = "${config.lib.stylix.colors.base03}";
+
+      # Text & UI
+      text = "${config.lib.stylix.colors.base05}";
+      subtext = "${config.lib.stylix.colors.base05}";
+      extratext = "${config.lib.stylix.colors.base05}";
       defaultFgColor = "${config.lib.stylix.colors.base05}";
+
+      # Buttons (vivid upgrades retained)
+      button = "${config.lib.stylix.colors.base0D}";
+      buttonActive = "${config.lib.stylix.colors.base0B}";
+      buttonDisabled = "${config.lib.stylix.colors.base01}";
+
+      # Tabs and misc
+      tabActive = "${config.lib.stylix.colors.base0E}";
+      misc = "${config.lib.stylix.colors.base03}";
+
+      # Notifications
+      notification = "${config.lib.stylix.colors.base09}";
+      notificationError = "${config.lib.stylix.colors.base08}";
     };
   };
 }
