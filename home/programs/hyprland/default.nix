68c75d6e7d23ed3f5865ac7bbbbb27cc49d8ae05
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-07-19 21:27:51 -0400

    move stuff to hm

--- home/programs/hyprland/default.nix
+++ home/programs/hyprland/default.nix
@@ -18,6 +18,10 @@
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
+  # For stuff between apps like clipboard access, drag and drop, etc.
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
+
   # Hyprland packages needed
   home.packages = with pkgs; [
     waybar
