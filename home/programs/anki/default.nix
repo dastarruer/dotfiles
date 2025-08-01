0278bf4053ecd1e36c541bd007bd6c2451ab83bf
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-08-01 14:46:16 -0400

    Update passfail2 config

--- home/programs/anki/default.nix
+++ home/programs/anki/default.nix
@@ -7,8 +7,8 @@
     (anki.withAddons (with ankiAddons; [
       (passfail2.withConfig {
         config = {
-          again_button_name = "Fail";
-          good_button_name = "Pass";
+          again_button_name = "Incorrect";
+          good_button_name = "Correct";
           again_button_textcolor = "#${config.lib.stylix.colors.base06}";
           good_button_textcolor = "#${config.lib.stylix.colors.base06}";
         };
