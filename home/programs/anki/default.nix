0dfbe305efe3e9861479edef880dd0066b647e6d
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-08-01 14:43:20 -0400

    update anki theme

--- home/programs/anki/default.nix
+++ home/programs/anki/default.nix
@@ -7,229 +7,323 @@
     (anki.withAddons (with ankiAddons; [
       (passfail2.withConfig {
         config = {
-          again_button_name = "Incorrect";
-          good_button_name = "Correct";
-          toggle_names_textcolors = 1;
+          again_button_name = "Fail";
+          good_button_name = "Pass";
+          again_button_textcolor = "#${config.lib.stylix.colors.base06}";
+          good_button_textcolor = "#${config.lib.stylix.colors.base06}";
         };
       })
       (recolor.withConfig {
         config = {
           colors = {
+            ACCENT_CARD = [
+              "Card mode"
+              "#${config.lib.stylix.colors.base0B}"
+              "#${config.lib.stylix.colors.base0B}"
+              "--accent-card"
+            ];
+            ACCENT_DANGER = [
+              "Danger"
+              "#${config.lib.stylix.colors.base08}"
+              "#${config.lib.stylix.colors.base08}"
+              "--accent-danger"
+            ];
+            ACCENT_NOTE = [
+              "Note mode"
+              "#${config.lib.stylix.colors.base0C}"
+              "#${config.lib.stylix.colors.base0C}"
+              "--accent-note"
+            ];
             BORDER = [
               "Border"
-              "#${config.lib.stylix.colors.base00}"
-              "#${config.lib.stylix.colors.base00}"
+              "#45475a"
+              "#45475a"
               "--border"
             ];
-            BURIED_FG = [
-              "Buried Foreground"
-              "#${config.lib.stylix.colors.base01}"
-              "#${config.lib.stylix.colors.base01}"
-              "--buried-fg"
+            BORDER_FOCUS = [
+              "Border (focused input)"
+              "#${config.lib.stylix.colors.base0D}"
+              "#${config.lib.stylix.colors.base0D}"
+              "--border-focus"
+            ];
+            BORDER_STRONG = [
+              "Border (strong)"
+              "#585b70"
+              "#585b70"
+              "--border-strong"
+            ];
+            BORDER_SUBTLE = [
+              "Border (subtle)"
+              "#313244"
+              "#313244"
+              "--border-subtle"
             ];
             BUTTON_BG = [
-              "Button Background"
-              "#${config.lib.stylix.colors.base02}"
-              "#${config.lib.stylix.colors.base02}"
+              "Button background"
+              "#313244"
+              "#313244"
               "--button-bg"
             ];
-            CURRENT_DECK = [
-              "Selected Deck"
-              "#${config.lib.stylix.colors.base03}"
-              "#${config.lib.stylix.colors.base03}"
-              "--current-deck"
+            BUTTON_DISABLED = [
+              "Button background (disabled)"
+              "#313244"
+              "#313244"
+              "--button-disabled"
             ];
-            DISABLED = [
-              "Disabled"
-              "#${config.lib.stylix.colors.base04}"
-              "#${config.lib.stylix.colors.base04}"
-              "--disabled"
+            BUTTON_HOVER = [
+              "Button background (hover)"
+              "#45475a"
+              "#45475a"
+              [
+                "--button-gradient-start"
+                "--button-gradient-end"
+              ]
             ];
-            FAINT_BORDER = [
-              "Faint Border"
-              "#${config.lib.stylix.colors.base05}"
-              "#${config.lib.stylix.colors.base05}"
-              "--faint-border"
+            BUTTON_HOVER_BORDER = [
+              "Button border (hover)"
+              "#585b70"
+              "#585b70"
+              "--button-hover-border"
             ];
-            FLAG1_BG = [
-              "Flag1 (Browse Cards List)"
-              "#${config.lib.stylix.colors.base06}"
-              "#${config.lib.stylix.colors.base06}"
-              "--flag1-bg"
+            BUTTON_PRIMARY_BG = [
+              "Button Primary Bg"
+              "#2f67e1"
+              "#2f67e1"
+              "--button-primary-bg"
             ];
-            FLAG1_FG = [
-              "Flag1 (Browse Sidebar)"
-              "#${config.lib.stylix.colors.base07}"
-              "#${config.lib.stylix.colors.base07}"
-              "--flag1-fg"
+            BUTTON_PRIMARY_DISABLED = [
+              "Button Primary Disabled"
+              "#4484ed"
+              "#4484ed"
+              "--button-primary-disabled"
             ];
-            FLAG2_BG = [
-              "Flag2 (Browse Cards List)"
-              "#${config.lib.stylix.colors.base08}"
-              "#${config.lib.stylix.colors.base08}"
-              "--flag2-bg"
+            BUTTON_PRIMARY_GRADIENT_END = [
+              "Button Primary Gradient End"
+              "#2544a8"
+              "#2544a8"
+              "--button-primary-gradient-end"
             ];
-            FLAG2_FG = [
-              "Flag2 (Browse Sidebar)"
-              "#${config.lib.stylix.colors.base09}"
-              "#${config.lib.stylix.colors.base09}"
-              "--flag2-fg"
+            BUTTON_PRIMARY_GRADIENT_START = [
+              "Button Primary Gradient Start"
+              "#2f67e1"
+              "#2f67e1"
+              "--button-primary-gradient-start"
             ];
-            FLAG3_BG = [
-              "Flag3 (Browse Cards List)"
-              "#${config.lib.stylix.colors.base0A}"
-              "#${config.lib.stylix.colors.base0A}"
-              "--flag3-bg"
-            ];
-            FLAG3_FG = [
-              "Flag3 (Browse Sidebar)"
-              "#${config.lib.stylix.colors.base0B}"
-              "#${config.lib.stylix.colors.base0B}"
-              "--flag3-fg"
-            ];
-            FLAG4_BG = [
-              "Flag4 (Browse Cards List)"
-              "#${config.lib.stylix.colors.base0C}"
-              "#${config.lib.stylix.colors.base0C}"
-              "--flag4-bg"
-            ];
-            FLAG4_FG = [
-              "Flag4 (Browse Sidebar)"
-              "#${config.lib.stylix.colors.base0D}"
-              "#${config.lib.stylix.colors.base0D}"
-              "--flag4-fg"
-            ];
-            FLAG5_BG = [
-              "Flag5 (Browse Cards List)"
-              "#${config.lib.stylix.colors.base0E}"
-              "#${config.lib.stylix.colors.base0E}"
-              "--flag5-bg"
-            ];
-            FLAG5_FG = [
-              "Flag5 (Browse Sidebar)"
-              "#${config.lib.stylix.colors.base0F}"
-              "#${config.lib.stylix.colors.base0F}"
-              "--flag5-fg"
-            ];
-            FLAG6_BG = [
-              "Flag6 (Browse Cards List)"
-              "#${config.lib.stylix.colors.base08}"
-              "#${config.lib.stylix.colors.base08}"
-              "--flag6-bg"
-            ];
-            FLAG6_FG = [
-              "Flag6 (Browse Sidebar)"
-              "#${config.lib.stylix.colors.base09}"
-              "#${config.lib.stylix.colors.base09}"
-              "--flag6-fg"
-            ];
-            FLAG7_BG = [
-              "Flag7 (Browse Cards List)"
-              "#${config.lib.stylix.colors.base0A}"
-              "#${config.lib.stylix.colors.base0A}"
-              "--flag7-bg"
-            ];
-            FLAG7_FG = [
-              "Flag7 (Browse Sidebar)"
-              "#${config.lib.stylix.colors.base0B}"
-              "#${config.lib.stylix.colors.base0B}"
-              "--flag7-fg"
-            ];
-            FRAME_BG = [
-              "Frame Background"
+            CANVAS = [
+              "Background"
               "#${config.lib.stylix.colors.base00}"
               "#${config.lib.stylix.colors.base00}"
-              "--frame-bg"
+              [
+                "--canvas"
+                "--bs-body-bg"
+              ]
+            ];
+            CANVAS_CODE = [
+              "Code editor background"
+              "#${config.lib.stylix.colors.base01}"
+              "#${config.lib.stylix.colors.base01}"
+              "--canvas-code"
+            ];
+            CANVAS_ELEVATED = [
+              "Review"
+              "#${config.lib.stylix.colors.base01}"
+              "#${config.lib.stylix.colors.base01}"
+              "--canvas-elevated"
+            ];
+            CANVAS_GLASS = [
+              "Background (transparent text surface)"
+              "#${config.lib.stylix.colors.base01}66"
+              "#${config.lib.stylix.colors.base01}66"
+              "--canvas-glass"
+            ];
+            CANVAS_INSET = [
+              "Background (inset)"
+              "#${config.lib.stylix.colors.base02}"
+              "#${config.lib.stylix.colors.base02}"
+              "--canvas-inset"
+            ];
+            CANVAS_OVERLAY = [
+              "Background (menu & tooltip)"
+              "#${config.lib.stylix.colors.base02}"
+              "#${config.lib.stylix.colors.base02}"
+              "--canvas-overlay"
+            ];
+            FG = [
+              "Text"
+              "#${config.lib.stylix.colors.base06}"
+              "#${config.lib.stylix.colors.base06}"
+              [
+                "--fg"
+                "--bs-body-color"
+              ]
+            ];
+            FG_DISABLED = [
+              "Text (disabled)"
+              "#${config.lib.stylix.colors.base05}"
+              "#${config.lib.stylix.colors.base05}"
+              "--fg-disabled"
+            ];
+            FG_FAINT = [
+              "Text (faint)"
+              "#${config.lib.stylix.colors.base04}"
+              "#${config.lib.stylix.colors.base04}"
+              "--fg-faint"
+            ];
+            FG_LINK = [
+              "Text (link)"
+              "#${config.lib.stylix.colors.base0D}"
+              "#${config.lib.stylix.colors.base0D}"
+              "--fg-link"
+            ];
+            FG_SUBTLE = [
+              "Text (subtle)"
+              "#${config.lib.stylix.colors.base03}"
+              "#${config.lib.stylix.colors.base03}"
+              "--fg-subtle"
+            ];
+            FLAG_1 = [
+              "Flag 1"
+              "#${config.lib.stylix.colors.base08}"
+              "#${config.lib.stylix.colors.base08}"
+              "--flag-1"
+            ];
+            FLAG_2 = [
+              "Flag 2"
+              "#${config.lib.stylix.colors.base09}"
+              "#${config.lib.stylix.colors.base09}"
+              "--flag-2"
+            ];
+            FLAG_3 = [
+              "Flag 3"
+              "#${config.lib.stylix.colors.base0A}"
+              "#${config.lib.stylix.colors.base0A}"
+              "--flag-3"
+            ];
+            FLAG_4 = [
+              "Flag 4"
+              "#${config.lib.stylix.colors.base0B}"
+              "#${config.lib.stylix.colors.base0B}"
+              "--flag-4"
+            ];
+            FLAG_5 = [
+              "Flag 5"
+              "#${config.lib.stylix.colors.base0D}"
+              "#${config.lib.stylix.colors.base0D}"
+              "--flag-5"
+            ];
+            FLAG_6 = [
+              "Flag 6"
+              "#${config.lib.stylix.colors.base0C}"
+              "#${config.lib.stylix.colors.base0C}"
+              "--flag-6"
+            ];
+            FLAG_7 = [
+              "Flag 7"
+              "#c9cbff"
+              "#c9cbff"
+              "--flag-7"
             ];
             HIGHLIGHT_BG = [
-              "Highlighted Background"
-              "#${config.lib.stylix.colors.base01}"
-              "#${config.lib.stylix.colors.base01}"
+              "Highlight background"
+              "#${config.lib.stylix.colors.base0F}"
+              "#${config.lib.stylix.colors.base0F}"
               "--highlight-bg"
             ];
             HIGHLIGHT_FG = [
-              "Highlighted Text"
-              "#${config.lib.stylix.colors.base07}"
-              "#${config.lib.stylix.colors.base07}"
+              "Highlight text"
+              "#${config.lib.stylix.colors.base0D}"
+              "#${config.lib.stylix.colors.base0D}"
               "--highlight-fg"
             ];
-            LEARN_COUNT = [
-              "Learn Count"
-              "#${config.lib.stylix.colors.base09}"
-              "#${config.lib.stylix.colors.base09}"
-              "--learn-count"
+            SCROLLBAR_BG = [
+              "Scrollbar background"
+              "#1e1e2e"
+              "#1e1e2e"
+              "--scrollbar-bg"
             ];
-            LINK = [
-              "Hyperlink"
-              "#${config.lib.stylix.colors.base0D}"
-              "#${config.lib.stylix.colors.base0D}"
-              "--link"
+            SCROLLBAR_BG_ACTIVE = [
+              "Scrollbar background (active)"
+              "#313244"
+              "#313244"
+              "--scrollbar-bg-active"
             ];
-            MARKED_BG = [
-              "Marked Background"
-              "#${config.lib.stylix.colors.base0A}"
-              "#${config.lib.stylix.colors.base0A}"
-              "--marked-bg"
-            ];
-            MEDIUM_BORDER = [
-              "Medium Border"
-              "#${config.lib.stylix.colors.base03}"
-              "#${config.lib.stylix.colors.base03}"
-              "--medium-border"
-            ];
-            NEW_COUNT = [
-              "New Count"
-              "#${config.lib.stylix.colors.base0C}"
-              "#${config.lib.stylix.colors.base0C}"
-              "--new-count"
-            ];
-            REVIEW_COUNT = [
-              "Review Count"
-              "#${config.lib.stylix.colors.base0B}"
-              "#${config.lib.stylix.colors.base0B}"
-              "--review-count"
-            ];
-            SLIGHTLY_GREY_TEXT = [
-              "Switch Text"
-              "#${config.lib.stylix.colors.base04}"
-              "#${config.lib.stylix.colors.base04}"
-              "--slightly-grey-text"
-            ];
-            SUSPENDED_BG = [
-              "Suspended Background"
-              "#${config.lib.stylix.colors.base0E}"
-              "#${config.lib.stylix.colors.base0E}"
-              "--suspended-bg"
-            ];
-            SUSPENDED_FG = [
-              "Suspended Foreground"
-              "#${config.lib.stylix.colors.base0E}"
-              "#${config.lib.stylix.colors.base0E}"
-              "--suspended-fg"
-            ];
-            TEXT_FG = [
-              "Text Foreground"
-              "#${config.lib.stylix.colors.base05}"
-              "#${config.lib.stylix.colors.base05}"
-              "--text-fg"
-            ];
-            TOOLTIP_BG = [
-              "Tooltip Background"
-              "#${config.lib.stylix.colors.base01}"
-              "#${config.lib.stylix.colors.base01}"
-              "--tooltip-bg"
-            ];
-            WINDOW_BG = [
-              "Window Background"
+            SCROLLBAR_BG_HOVER = [
+              "Scrollbar background (hover)"
               "#${config.lib.stylix.colors.base00}"
               "#${config.lib.stylix.colors.base00}"
-              "--window-bg"
+              "--scrollbar-bg-hover"
             ];
-            ZERO_COUNT = [
-              "Zero Count"
-              "#${config.lib.stylix.colors.base03}"
-              "#${config.lib.stylix.colors.base03}"
-              "--zero-count"
+            SELECTED_BG = [
+              "Selected Bg"
+              "#313244"
+              "#313244"
+              "--selected-bg"
+            ];
+            SELECTED_FG = [
+              "Selected Fg"
+              "#${config.lib.stylix.colors.base0A}"
+              "#${config.lib.stylix.colors.base0A}"
+              "--selected-fg"
+            ];
+            SHADOW = [
+              "Shadow"
+              "#1e1e2e"
+              "#1e1e2e"
+              "--shadow"
+            ];
+            SHADOW_FOCUS = [
+              "Shadow  =focused input)"
+              "#${config.lib.stylix.colors.base0D}"
+              "#${config.lib.stylix.colors.base0D}"
+              "--shadow-focus"
+            ];
+            SHADOW_INSET = [
+              "Shadow (inset)"
+              "#11111b"
+              "#11111b"
+              "--shadow-inset"
+            ];
+            SHADOW_SUBTLE = [
+              "Shadow (subtle)"
+              "#181825"
+              "#181825"
+              "--shadow-subtle"
+            ];
+            STATE_BURIED = [
+              "Buried"
+              "#7f849c"
+              "#7f849c"
+              "--state-buried"
+            ];
+            STATE_LEARN = [
+              "Learn"
+              "#${config.lib.stylix.colors.base08}"
+              "#${config.lib.stylix.colors.base08}"
+              "--state-learn"
+            ];
+            STATE_MARKED = [
+              "Marked"
+              "#${config.lib.stylix.colors.base09}"
+              "#${config.lib.stylix.colors.base09}"
+              "--state-marked"
+            ];
+            STATE_NEW = [
+              "New"
+              "#${config.lib.stylix.colors.base0C}"
+              "#${config.lib.stylix.colors.base0C}"
+              "--state-new"
+            ];
+            STATE_REVIEW = [
+              "Review"
+              "#${config.lib.stylix.colors.base0D}"
+              "#${config.lib.stylix.colors.base0D}"
+              "--state-review"
+            ];
+            STATE_SUSPENDED = [
+              "Suspended"
+              "#${config.lib.stylix.colors.base06}"
+              "#${config.lib.stylix.colors.base06}"
+              "--state-suspended"
             ];
           };
           version = {
