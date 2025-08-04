f92b018e3c229d5ea01fad71fa81aa68983b0352
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-04 13:27:42 -0400

    stuff

--- nix/modules/default.nix
+++ nix/modules/default.nix
@@ -1,12 +1,19 @@
 {...}: {
   imports = [
     ./desktops/hyprland
-    ./system
-    ./services
-    ./users
-
+    ./boot
+    ./battery.nix
+    ./bluetooth.nix
+    ./calibre.nix
+    # ./kavita.nix
+    ./keyboard.nix
+    ./locale.nix
+    ./maintenance.nix
+    ./networking.nix
+    ./power-button.nix
     ./stylix.nix
     ./system-packages.nix
     ./secrets.nix
+    ./users.nix
   ];
 }
