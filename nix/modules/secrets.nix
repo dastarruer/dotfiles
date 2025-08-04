a0b2dd097bdb319a76f1ce0d12f1efcc2c8aa219
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-08-04 12:51:33 -0400

    stuff

--- nix/modules/secrets.nix
+++ nix/modules/secrets.nix
@@ -14,11 +14,15 @@
     age.keyFile = "/home/dastarruer/.config/sops/age/keys.txt";
 
     # Ssh key
-    secrets.ssh = {
-      owner = config.users.users.dastarruer.name;
-      group = config.users.users.dastarruer.group;
-      path = "/home/dastarruer/.ssh/id_ed25519";
-      mode = "0600"; # Only owner can read/write
+    secrets = {
+      ssh = {
+        owner = config.users.users.dastarruer.name;
+        group = config.users.users.dastarruer.group;
+        path = "/home/dastarruer/.ssh/id_ed25519";
+        mode = "0600"; # Only owner can read/write
+      };
+
+      github_recovery_codes = {};
     };
   };
 }
