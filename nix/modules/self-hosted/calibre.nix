3ad904b32531bdecfd26946da2b065f9a00c0175
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-05 21:47:09 -0400

    proper calibre support

--- nix/modules/self-hosted/calibre.nix
+++ nix/modules/self-hosted/calibre.nix
@@ -6,14 +6,17 @@
     # Very secure i know but i cant be bothered to figure out how to give calibre-web access to $HOME
     user = "dastarruer";
 
-    # Enabling this means metadata.db will be read only. this is a stupid thing and i hate it
-    # options = {
-    #   calibreLibrary = /home/dastarruer/Documents/calibre;
-    # };
-  };
+    listen = {
+      ip = "0.0.0.0"; # bind to all interfaces so other devices can connect
+      port = 8083;
+    };
 
-  # chatgpt's solution to symlink ~/Documents/calibre to /srv/calibre, while giving calibre access to /srv/calibre
-  # systemd.tmpfiles.rules = [
-  #   "L /srv/calibre - - - - /home/dastarruer/Documents/calibre"
-  # ];
+    options = {
+      # Enabling this means metadata.db will be read only. this is a stupid thing and i hate it
+      # calibreLibrary = /home/dastarruer/Documents/calibre;
+
+      enableBookConversion = true;
+      enableBookUploading = true;
+    };
+  };
 }
