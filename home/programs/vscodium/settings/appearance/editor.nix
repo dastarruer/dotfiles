26af95f5eedbb2a9dfcc10f3be4bcb75324a50aa
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-11 19:24:15 -0400

    stuff

--- home/programs/vscodium/settings/appearance/editor.nix
+++ home/programs/vscodium/settings/appearance/editor.nix
@@ -3,7 +3,7 @@
   programs.vscode.profiles.default.userSettings = {
     "editor.lightbulb.enabled" = "off";
     "editor.guides.indentation" = false;
-    "editor.scrollbar.vertical" = "hidden";
+    "editor.scrollbar.vertical" = "auto";
     "editor.scrollbar.horizontal" = "hidden";
     "editor.overviewRulerBorder" = false;
     "editor.hideCursorInOverviewRuler" = true;
