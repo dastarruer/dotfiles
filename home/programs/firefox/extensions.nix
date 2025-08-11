9cdfac3d51ba9796b2a7e5fd9d1b1f972147a05d
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-11 19:12:09 -0400

    add stylix to firefox kinda

--- home/programs/firefox/extensions.nix
+++ home/programs/firefox/extensions.nix
@@ -2,17 +2,22 @@
   # Folder under which all firefox stuff goes
   firefoxProfile = "default";
 in {
-  # Declare a bunch of extensinos
-  programs.firefox.profiles.${firefoxProfile}.extensions.packages = with inputs.firefox-addons.packages."x86_64-linux"; [
-    ublock-origin
-    sponsorblock
-    darkreader
-    pywalfox
-    simple-tab-groups
-    startpage-private-search
-    i-dont-care-about-cookies
-    privacy-badger
-    link-cleaner
-    bitwarden
-  ];
+  programs.firefox.profiles.${firefoxProfile}.extensions = {
+    # Required for stylix
+    force = true;
+
+    # Declare a bunch of extensinos
+    packages = with inputs.firefox-addons.packages."x86_64-linux"; [
+      ublock-origin
+      sponsorblock
+      darkreader
+      pywalfox
+      simple-tab-groups
+      startpage-private-search
+      i-dont-care-about-cookies
+      privacy-badger
+      link-cleaner
+      bitwarden
+    ];
+  };
 }
