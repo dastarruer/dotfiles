8565da16102d5dabab4b6e002e3f8f4ca7f3b4e7
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-11 23:47:33 -0400

    stuff

--- home/programs/hyprland/plugins.nix
+++ home/programs/hyprland/plugins.nix
@@ -16,8 +16,8 @@
           add_borders = 1; # 0 - 9
 
           # you can add up to 9 borders
-          col.border_1 = "rgb(ffffff)";
-          col.border_2 = "rgb(2222ff)";
+          "col.border_1" = "rgb(ffffff)";
+          "col.border_2" = "rgb(2222ff)";
 
           # -1 means "default" as in the one defined in general:border_size
           border_size_1 = 10;
