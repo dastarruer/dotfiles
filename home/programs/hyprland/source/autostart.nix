b03a2d1c15abe1b8956ceb9348bded74189e9b95
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-11 23:10:19 -0400

    stuff

--- home/programs/hyprland/source/autostart.nix
+++ home/programs/hyprland/source/autostart.nix
@@ -1,4 +1,4 @@
-{...}: {
+{config, ...}: {
   wayland.windowManager.hyprland.settings.exec-once = [
     "waybar"
     "swww-daemon"
@@ -16,5 +16,8 @@
     # Start clipboard manager
     "wl-paste --type text --watch cliphist store"
     "wl-paste --type image --watch cliphist store"
+
+    # Start smart-pause-resume script
+    "${config.home.homeDirectory}/bin/smart-pause-resume.sh"
   ];
 }
