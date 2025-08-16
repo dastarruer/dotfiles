01e29197fa286b45af83fbd4ef711a7d13fa3024
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-16 19:23:22 -0400

    remove unnecessary xdg portal declaration

--- home/programs/hyprland/default.nix
+++ home/programs/hyprland/default.nix
@@ -15,12 +15,10 @@
 
     # set to the flake package for more up to date software
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+
+    # For stuff between apps like clipboard access, drag and drop, etc.
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
-
-  # For stuff between apps like clipboard access, drag and drop, etc.
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
 
   # Hyprland packages needed
   home.packages = with pkgs; [
