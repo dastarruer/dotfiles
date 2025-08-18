537dff38fffd6001b968b7a599482922219f1b64
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-18 15:51:02 -0400

    stuff

--- nix/modules/bluetooth.nix
+++ nix/modules/bluetooth.nix
@@ -8,9 +8,12 @@
 
     # Set bluetooth settings
     settings = {
-      # For airpods (https://gist.github.com/aidos-dev/b49078c1d8c6bb1621e4ac199d18213b)
       General = {
+        # For airpods (https://gist.github.com/aidos-dev/b49078c1d8c6bb1621e4ac199d18213b)
         ControllerMode = "bredr";
+
+        # Lets devices connect faster at the cost of battery life
+        # FastConnectable = true;
       };
     };
   };
