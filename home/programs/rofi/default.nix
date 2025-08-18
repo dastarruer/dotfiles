35075031aa961ef51474fbc521ca79e2780741f1
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-18 10:44:46 -0400

    add rofi config

--- home/programs/rofi/default.nix
+++ home/programs/rofi/default.nix
@@ -1,6 +1,9 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [
-    ./config.nix
     ./plugins
   ];
 
@@ -10,4 +13,20 @@
     # Specify rofi-wayland because wayland
     package = pkgs.rofi-wayland;
   };
+
+  # Symlink the rofi config
+  # Do this manually because apparently symlinking a directory is too hard for home manager...
+  # Rofi config comes from here: https://github.com/Spelljinxer/dotfiles/blob/main/.config/rofi/
+  home.activation.linkRofi = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    SRC="$HOME/.dotfiles/config/rofi"
+    DEST="$HOME/.config/rofi"
+
+    # Remove existing symlink or directory
+    if [ -e "$DEST" ]; then
+        rm -rf "$DEST"
+    fi
+
+    # Create symlink
+    ln -s "$SRC" "$DEST"
+  '';
 }
