3a9dfb138e8059c8f3cca0d6315ce40d463de76f
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-18 12:26:18 -0400

    modify battery-status

---
+++ config/bin/battery-status.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+status="$(cat /sys/class/power_supply/BAT0/status)"
+level="$(cat /sys/class/power_supply/BAT0/capacity)"
+
+if [[ "$status" == "Discharging" || "$status" == "Full"]]; then
+  if [[ "$level" -eq 0 ]]; then
+    echo "$level% "
+  elif [[ "$level" -ge 1 && "$level" -le 25 ]]; then
+    echo "$level% "
+  elif [[ "$level" -ge 26 && "$level" -le 50 ]]; then
+    echo "$level% "
+  elif [[ "$level" -ge 51 && "$level" -le 75 ]]; then
+    echo "$level% "
+  elif [[ "$level" -ge 76 && "$level" -le 100 ]]; then
+    echo "$level% "
+  fi
+elif [[ "$status" == "Charging" ]]; then
+  echo "$level "
+fi
