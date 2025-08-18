c4f5b6e99bd051223d8bc41d25a67551736a2b7f
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-18 11:15:37 -0400

    add theme.rasi to gitignore

--- home/programs/rofi/theme.nix
+++ home/programs/rofi/theme.nix
@@ -2,12 +2,12 @@
   # Generate the theme.rasi file
   home.file.".dotfiles/config/rofi/themes/theme.rasi".text = ''
     * {
-      main-bg:    ${config.lib.stylix.colors.base00};
-      main-fg:    ${config.lib.stylix.colors.base05};
-      main-br:    ${config.lib.stylix.colors.base0A};
-      main-ex:    ${config.lib.stylix.colors.base0D};
-      select-bg:  ${config.lib.stylix.colors.base0F};
-      select-fg:  ${config.lib.stylix.colors.base03};
+      main-bg:    #${config.lib.stylix.colors.base00};
+      main-fg:    #${config.lib.stylix.colors.base05};
+      main-br:    #${config.lib.stylix.colors.base0A};
+      main-ex:    #${config.lib.stylix.colors.base0D};
+      select-bg:  #${config.lib.stylix.colors.base0F};
+      select-fg:  #${config.lib.stylix.colors.base03};
     }
   '';
 }
