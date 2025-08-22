3810eb7d9739b7d65cc43285d1571cbb926963bc
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-22 19:45:22 -0400

    stuff

--- home/programs/anki/default.nix
+++ home/programs/anki/default.nix
@@ -15,16 +15,17 @@ in {
       (recolor.withConfig {
         config = theme;
       })
+
       # Image occlusion enhanced
       (pkgs.anki-utils.buildAnkiAddon (finalAttrs: {
         pname = "image-occlusion-enhanced";
-        # To find the version, run:
+        # To find the version, run: git ls-remote --tags https://github.com/glutanimate/image-occlusion-enhanced.git
         version = "v1.4.0";
         src = pkgs.fetchFromGitHub {
           owner = "glutanimate";
           repo = "image-occlusion-enhanced";
           rev = finalAttrs.version;
-          sparseCheckout = ["image_occlusion_enhanced"];
+          sparseCheckout = ["src/image_occlusion_enhanced"];
           hash = "sha256-YR1hicBDb08J+1Qc+SDiJDXLo5FzLqCQGeVe7brbPME=";
         };
         sourceRoot = "${finalAttrs.src.name}/src/image_occlusion_enhanced";
