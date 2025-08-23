6bf02693b6c6720a930758409b23eceb372c8e1d
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-22 20:22:30 -0400

    add shadow color

--- home/programs/hyprland/colors.nix
+++ home/programs/hyprland/colors.nix
@@ -5,5 +5,11 @@
       col.active_border = rgb(${config.lib.stylix.colors.base05})
       col.inactive_border = rgb(${config.lib.stylix.colors.base03})
     }
+
+    decoration {
+      shadow {
+        color = rgb(${config.lib.stylix.colors.base00})
+      }
+    }
   '';
 }
