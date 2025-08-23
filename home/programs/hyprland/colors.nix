a0b2a1ec00e7f270adf0cd964d7a6ee4a2a6fe54
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-22 20:16:52 -0400

    fix colors hyprland

--- home/programs/hyprland/colors.nix
+++ home/programs/hyprland/colors.nix
@@ -1,27 +1,9 @@
 {config, ...}: {
   # Generate a colors.conf file using Stylix base16 colors
-  # This gets symlinked in default.nix
-  home.file.".dotfiles/config/hypr/hyprland/colors.conf".text = ''
-    col.bg0 = ${config.lib.stylix.colors.base00}
-    col.bg1 = ${config.lib.stylix.colors.base01}
-    col.bg2 = ${config.lib.stylix.colors.base02}
-    col.bg3 = ${config.lib.stylix.colors.base03}
-    col.fg0 = ${config.lib.stylix.colors.base05}
-    col.fg1 = ${config.lib.stylix.colors.base06}
-    col.fg2 = ${config.lib.stylix.colors.base07}
-    col.red = ${config.lib.stylix.colors.base08}
-    col.green = ${config.lib.stylix.colors.base0B}
-    col.yellow = ${config.lib.stylix.colors.base0A}
-    col.blue = ${config.lib.stylix.colors.base0D}
-    col.magenta = ${config.lib.stylix.colors.base0E}
-    col.cyan = ${config.lib.stylix.colors.base0C}
-    col.orange = ${config.lib.stylix.colors.base09}
-    col.active_border = ${config.lib.stylix.colors.base0C}
-    col.inactive_border = ${config.lib.stylix.colors.base03}
-    groupbar.col.active = ${config.lib.stylix.colors.base0C}
-    groupbar.col.inactive = ${config.lib.stylix.colors.base03}
-    groupbar.text_color = ${config.lib.stylix.colors.base05}
-    shadow.color = ${config.lib.stylix.colors.base0C}99
-    misc.background_color = ${config.lib.stylix.colors.base00}
+  home.file.".cache/hypr/colors.conf".text = ''
+    general {
+      col.active_border = rgb(${config.lib.stylix.colors.base05})
+      col.inactive_border = rgb(${config.lib.stylix.colors.base03})
+    }
   '';
 }
