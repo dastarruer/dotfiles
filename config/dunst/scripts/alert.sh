bc333d3e60d5a4d3ea9eaafaaed05c4a01728509
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-25 17:41:12 -0400

    add flameshot to blocked notifs for alert sound

--- config/dunst/scripts/alert.sh
+++ config/dunst/scripts/alert.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if [ "$DUNST_BODY" != "^Volume" ] && [ "$DUNST_BODY" != "^Brightness" ] && [ "$DUNST_APP_NAME" != "Spotify" ]; then
+if [ "$DUNST_BODY" != "^Volume" ] && [ "$DUNST_BODY" != "^Brightness" ] && [ "$DUNST_APP_NAME" != "Spotify" ] && [ "$DUNST_APP_NAME" != "flameshot" ]; then
     pw-play $HOME/.config/dunst/alerts/default.wav
 fi
