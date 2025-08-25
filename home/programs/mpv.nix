1d5ae93e8a3832bffd2e1528016e124c181d0927
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-25 07:32:51 -0400

    Add mpv conf

--- home/programs/mpv.nix
+++ home/programs/mpv.nix
@@ -1,3 +1,16 @@
-{...}: {
+{config, ...}: {
   programs.mpv.enable = true;
+
+  # Without this, home manager can't symlink files to .config (https://github.com/nix-community/home-manager/issues/1807#issuecomment-3131623755)
+  xdg.configFile = {
+    "mpv/mpv.conf".enable = false;
+  };
+
+  # Symlink the rofi config
+  home.file.".config/mpv/mpv.conf" = {
+    source =
+      config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/.dotfiles/config/mpv/mpv.conf";
+    recursive = true;
+  };
 }
