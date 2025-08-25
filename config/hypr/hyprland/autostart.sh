d1b774c81b63257822257e259ff2e5370f2227ea
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-24 22:37:42 -0400

    add autostart.sh

---
+++ config/hypr/hyprland/autostart.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+waybar &
+swww-daemon &
+nm-applet &
+hypridle &
+
+# Start apps
+todoist-electron &
+anki &
+spotify &
+firefox &
+codium &
+flatpak run md.obsidian.Obsidian &
+
+# Start clipboard manager
+wl-paste --type text --watch cliphist store &
+wl-paste --type image --watch cliphist store &
