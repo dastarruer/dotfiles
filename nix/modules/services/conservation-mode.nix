9d72160847c0c0990c5b94b0f964489797374fd1
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-31 23:47:09 -0400

    stuff

--- nix/modules/services/conservation-mode.nix
+++ nix/modules/services/conservation-mode.nix
@@ -3,37 +3,35 @@
   boot.kernelModules = ["ideapad_laptop"];
 
   # A systemd service to auto enable conservation mode on my home wifi, and disable it when connected to anything else
-  systemd = {
-    services.conservation-mode = {
-      # Lets the service use packages here
-      path = with pkgs; [
-        usbutils # for lsusb
-        bash
-      ];
+  systemd.services.conservation-mode = {
+    # Lets the service use packages here
+    path = with pkgs; [
+      usbutils # for lsusb
+      bash
+    ];
 
-      description = "Toggle Lenovo IdeaPad conservation mode based on whether the mouse is plugged in";
+    description = "Toggle Lenovo IdeaPad conservation mode based on whether the mouse is plugged in";
 
-      # Start on boot
-      wantedBy = ["multi-user.target"];
+    # Start on boot
+    wantedBy = ["multi-user.target"];
 
-      # Wait for network access
-      after = ["NetworkManager.service"];
+    # Wait for network access
+    after = ["NetworkManager.service"];
 
-      # Run the service before suspend
-      before = ["sleep.target"];
+    # Run the service before suspend
+    before = ["sleep.target"];
 
-      serviceConfig = {
-        Type = "oneshot";
-        ExecStart = ./scripts/conservation-mode.sh;
-      };
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = ./scripts/conservation-mode.sh;
     };
+  };
 
-    timers.conservation-mode = {
-      wantedBy = ["multi-user.target"];
-      timerConfig = {
-        # Trigger every minute
-        OnCalendar = "minutely";
-      };
+  systemd.timers.conservation-mode = {
+    wantedBy = ["multi-user.target"];
+    timerConfig = {
+      # Trigger every minute
+      OnCalendar = "minutely";
     };
   };
 }
