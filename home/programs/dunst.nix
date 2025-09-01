fd6611b7b8ca71c40e0ab8ced8d8e83a46154b2d
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-09-01 13:42:17 -0400

    stuff

--- home/programs/dunst.nix
+++ home/programs/dunst.nix
@@ -23,7 +23,7 @@
         frame_width = 2;
 
         # Icon Settings
-        icon_theme = "Papirus-Dark";
+        icon_theme = "${config.gtk.iconTheme.name}";
         icon_corner_radius = 0;
         min_icon_size = 80;
         max_icon_size = 100;
