6e8d528bfdf2966c4ad3edc6bde30051f78b4f33
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-08-31 23:27:06 -0400

    stuff

--- home.nix
+++ home.nix
@@ -18,6 +18,12 @@
     stateVersion = "22.11";
   };
 
+  # Set some necessary sops settings
+  sops = {
+    age.keyFile = "/home/dastarruer/.config/sops/age/keys.txt";
+    defaultSopsFile = ./secrets/secrets.yaml;
+  };
+
   # Disable stylix configuration for certain apps
   stylix.targets = {
     # Note that these requires manual theming
--- home/services/scripts/cleanup-downloads.sh
+++ home/services/scripts/cleanup-downloads.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+# This is a necessary wrapper around notify-send in order to send notifications from a systemd service
+notify() {
+    local title=$1
+    local message=$2
+
+    if [ -n "${DISPLAY-}" ] || [ -n "${WAYLAND_DISPLAY-}" ]; then
+        # GUI session available
+         DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$USERID/bus" \
+            notify-send "$title" "$message"
+    else
+        # Headless / systemd run → fallback to stderr
+        echo "[$title] $message" >&2
+    fi
+}
+
+notify "Clearing downloads directory..."
+
 # Delete files older than five days in the downloads dir
 find $HOME/Downloads -mindepth 1 -mtime +5 -delete
 
+notify "Cleared downloads directory."
