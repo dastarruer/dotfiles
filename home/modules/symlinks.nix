7ccb0f1197a856a68f5f2a5f021b635325193f21
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-09-07 13:12:44 -0400

    symlink catppuccin wallpapers

--- home/modules/symlinks.nix
+++ home/modules/symlinks.nix
@@ -1,6 +1,6 @@
 {
   config,
-  # lib,
+  lib,
   ...
 }: {
   # Symlink a bunch of files
@@ -20,62 +20,14 @@
         "${config.home.homeDirectory}/.dotfiles/config/bashrc/.bashrc";
     };
     "Pictures/wallpapers" = {
-      source =
+      source = lib.mkDefault (
         config.lib.file.mkOutOfStoreSymlink
-        "${config.home.homeDirectory}/.dotfiles/config/wallpapers/gruvbox";
+        "${config.home.homeDirectory}/.dotfiles/config/wallpapers/gruvbox"
+      );
     };
   };
-}
-# UNUSED
-# ".gitconfig" = {
-#   source =
-#     config.lib.file.mkOutOfStoreSymlink
-#     "${config.home.homeDirectory}/.dotfiles/gitconfig/.gitconfig";
-# };
-# ".gitignore_global" = {
-#   source =
-#     config.lib.file.mkOutOfStoreSymlink
-#     "${config.home.homeDirectory}/.dotfiles/gitconfig/.gitignore_global";
-# };
-# .config files
-# ".config/hypr" = {
-#   source =
-#     config.lib.file.mkOutOfStoreSymlink
-#     "${config.home.homeDirectory}/.dotfiles/hypr";
-# };
-# ".config/kitty" = {
-#   source =
-#     config.lib.file.mkOutOfStoreSymlink
-#     "${config.home.homeDirectory}/.dotfiles/kitty";
-# };
-# ".config/neofetch" = {
-#   source =
-#     config.lib.file.mkOutOfStoreSymlink
-#     "${config.home.homeDirectory}/.dotfiles/neofetch";
-# };
-# ".config/wal" = {
-#   source =
-#     config.lib.file.mkOutOfStoreSymlink
-#     "${config.home.homeDirectory}/.dotfiles/pywal";
-# };
-# ".config/rofi" = {
-#   source =
-#     config.lib.file.mkOutOfStoreSymlink
-#     "${config.home.homeDirectory}/.dotfiles/rofi";
-# };
-# ".config/starship" = {
-#   source =
-#     config.lib.file.mkOutOfStoreSymlink
-#     "${config.home.homeDirectory}/.dotfiles/starship";
-# };
-# ".config/waybar" = {
-#   source =
-#     config.lib.file.mkOutOfStoreSymlink
-#     "${config.home.homeDirectory}/.dotfiles/waybar";
-# };
-# ".config/yazi" = {
-#   source =
-#     config.lib.file.mkOutOfStoreSymlink
-#     "${config.home.homeDirectory}/.dotfiles/yazi";
-# };
 
+  specialisation.catppuccin.configuration.home.file."Pictures/wallpapers".source =
+    config.lib.file.mkOutOfStoreSymlink
+    "${config.home.homeDirectory}/.dotfiles/config/wallpapers/catppuccin";
+}
