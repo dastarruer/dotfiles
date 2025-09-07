1f7008a857d9f31a39a41333bc74542c659fc93a
/home/dastarruer/.config/sops-nix/secrets/name </home/dastarruer/.config/sops-nix/secrets/email>
2025-09-06 22:02:49 -0400

    move vscode extensions to proper place

--- home/modules/programs/vscodium/extensions.nix
+++ home/modules/programs/vscodium/extensions.nix
@@ -3,24 +3,9 @@
   inputs,
   ...
 }: {
-  # Define vscode extensions
+  # Vscode extensions that I want globally
   programs.vscode.profiles.default.extensions = with inputs.vscode-extensions.extensions.${pkgs.stdenv.hostPlatform.system}.vscode-marketplace; [
-    # Formatters
-    kamadorueda.alejandra
-    esbenp.prettier-vscode
-    foxundermoon.shell-format
-
-    # Appearance
-    pkief.material-icon-theme
-    jdinhlife.gruvbox
-
-    # Other
     aaron-bond.better-comments
     usernamehw.errorlens
-  ];
-
-  # Add necessary packages for each extension
-  home.packages = with pkgs; [
-    rustfmt
   ];
 }
--- home/modules/programs/vscodium/languages/default.nix
+++ home/modules/programs/vscodium/languages/default.nix
@@ -1,4 +1,8 @@
-{...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   # Import language specific options
   imports = [
     ./nix
@@ -6,27 +10,33 @@
   ];
 
   # General language formatter settings
-  programs.vscode.profiles.default.userSettings = {
-    "[javascript]" = {
-      editor.defaultFormatter = "esbenp.prettier-vscode";
-    };
-    "[css]" = {
-      editor.defaultFormatter = "esbenp.prettier-vscode";
-    };
-    "[html]" = {
-      editor.defaultFormatter = "esbenp.prettier-vscode";
-    };
-    "[json]" = {
-      editor.defaultFormatter = "esbenp.prettier-vscode";
-    };
-    "[go]" = {
-      editor.defaultFormatter = "golang.go";
-    };
-    "[shellscript]" = {
-      editor.defaultFormatter = "foxundermoon.shell-format";
-    };
-    "[ignore]" = {
-      editor.defaultFormatter = "foxundermoon.shell-format";
+  programs.vscode.profiles.default = {
+    extensions = with inputs.vscode-extensions.extensions.${pkgs.stdenv.hostPlatform.system}.vscode-marketplace; [
+      esbenp.prettier-vscode
+      # foxundermoon.shell-format
+    ];
+    userSettings = {
+      "[javascript]" = {
+        editor.defaultFormatter = "esbenp.prettier-vscode";
+      };
+      "[css]" = {
+        editor.defaultFormatter = "esbenp.prettier-vscode";
+      };
+      "[html]" = {
+        editor.defaultFormatter = "esbenp.prettier-vscode";
+      };
+      "[json]" = {
+        editor.defaultFormatter = "esbenp.prettier-vscode";
+      };
+      "[go]" = {
+        editor.defaultFormatter = "golang.go";
+      };
+      # "[shellscript]" = {
+      #   editor.defaultFormatter = "foxundermoon.shell-format";
+      # };
+      "[ignore]" = {
+        editor.defaultFormatter = "foxundermoon.shell-format";
+      };
     };
   };
 }
