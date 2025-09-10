e099bdda7e776bd4063054d858c91f88a292deb4
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-09-10 10:55:22 -0400

    autoExpire hm

--- home/modules/maintenance.nix
+++ home/modules/maintenance.nix
@@ -9,4 +9,14 @@
     # Very rarely do i rollback so here ig... will speed up boot too
     options = "--delete-older-than 2d";
   };
+
+  # Periodically expire hm generations, idk if nix.gc makes this redundant though
+  services.home-manager.autoExpire = {
+    enable = true;
+    frequency = "daily";
+    store.cleanup = true;
+
+    # Remove all generations older than 2 days
+    timestamp = "-2 days";
+  };
 }
