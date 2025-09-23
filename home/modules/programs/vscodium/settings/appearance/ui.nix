b697b89d6964e53b81196d1572053cb2aa5488a4
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-09-23 12:08:37 -0400

    add activity bar back

--- home/modules/programs/vscodium/settings/appearance/ui.nix
+++ home/modules/programs/vscodium/settings/appearance/ui.nix
@@ -2,7 +2,7 @@
   # UI Layout & Navigation
   programs.vscode.profiles.default.userSettings = {
     "workbench.sideBar.location" = "right";
-    "workbench.activityBar.location" = "hidden";
+    "workbench.activityBar.location" = "top";
     "workbench.statusBar.visible" = false;
     "workbench.editor.showTabs" = "single";
     "workbench.startupEditor" = "none";
