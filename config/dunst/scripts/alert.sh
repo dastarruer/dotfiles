f31d3eb0d864a240efea2408ab3059a522f30464
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-09-27 13:40:34 -0400

    rmeove alerts on wallpaper change

--- config/dunst/scripts/alert.sh
+++ config/dunst/scripts/alert.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-if [ "$DUNST_APP_NAME" != "Spotify" ] && [ "$DUNST_APP_NAME" != "flameshot" ]; then
-    pw-play $HOME/.config/dunst/alerts/default.wav
+if [ "$DUNST_APP_NAME" != "Spotify" ] && \
+   [ "$DUNST_APP_NAME" != "flameshot" ] && \
+   [[ "$DUNST_SUMMARY" != *"Wallpaper set to:"* ]]; then
+    pw-play "$HOME/.config/dunst/alerts/default.wav"
 fi
