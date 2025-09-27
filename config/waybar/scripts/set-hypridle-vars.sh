fa27fd8ac0d81ef601e6bd7d23da684c1c4e5edd
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-09-27 13:28:38 -0400

    add option to disable hypridle suspend

--- config/waybar/scripts/set-hypridle-vars.sh
+++ config/waybar/scripts/set-hypridle-vars.sh
@@ -10,18 +10,27 @@ fi
 # Path to hypridle file
 config_file="$HOME/.config/hypr/hypridle.conf"
 
-# If the variable already exists, replace it. Otherwise append it.
-if grep -q "^\$suspend_secs" "$config_file"; then
-  sed -i "s/^\$suspend_secs = .*/\$suspend_secs = $1/" "$config_file"
-else
-  echo "\$suspend_secs = $1" >> "$config_file"
-fi
+case "$1" in
+  0)
+    ln -sf "$config_file/no-suspend.conf" "$HOME/.config/hypr/hypridle.conf"
+    notify-send "Idle suspend disabled"
+    ;;
+  5)
+    ln -sf "$config_file/5min.conf" "$HOME/.config/hypr/hypridle.conf"
+    notify-send "Will suspend after 5 minutes"
+    ;;
+  30)
+    ln -sf "$config_file/30min.conf" "$HOME/.config/hypr/hypridle.conf"
+    notify-send "Will suspend after 30 minutes"
+    ;;
+  *)
+    echo "Error: Invalid value '$1'. Allowed values: 0, 5, 30"
+    exit 1
+    ;;
+esac
 
 # Track current selection for Waybar
 echo "$1" > "$XDG_RUNTIME_DIR/hypridle-timeout"
 
 # Restart hypridle
 systemctl restart hypridle.service --user
-
-notify-send "Will suspend after $(($1 / 60)) minutes"
-
