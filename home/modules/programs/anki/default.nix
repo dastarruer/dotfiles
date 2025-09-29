13861fd6e2353ff86e075a1293ded57396259c16
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-09-28 22:04:35 -0400

    add anki sync settings

--- home/modules/programs/anki/default.nix
+++ home/modules/programs/anki/default.nix
@@ -1,11 +1,21 @@
 # Check here for packaging an anki addon: https://github.com/NixOS/nixpkgs/blob/master/pkgs/games/anki/with-addons.nix
-{...}: {
+{config, ...}: {
   imports = [
     ./addons
   ];
 
+  sops.secrets.anki_synckey = {};
+
   programs.anki = {
     enable = true;
     theme = "dark";
+
+    sync = {
+      autoSync = true;
+      syncMedia = true;
+
+      keyFile = config.sops.secrets.anki_synckey.path;
+      usernameFile = config.sops.secrets.email.path;
+    };
   };
 }
