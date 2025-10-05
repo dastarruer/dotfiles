e2e3ebc923b5e7b7c5a7f9caf311ce457cba3c93
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-05 15:07:42 -0400

    add notification upon ludusavi backup

--- home/modules/programs/gaming/ludusavi.nix
+++ home/modules/programs/gaming/ludusavi.nix
@@ -10,7 +10,11 @@
     };
 
     Service = {
-      ExecStart = "${pkgs.ludusavi}/bin/ludusavi backup --force";
+      ExecStart = "${pkgs.writeShellScript "ludusavi-backup" ''
+        #!/run/current-system/sw/bin/bash
+        ${pkgs.libnotify}/bin/notify-send "It's that time of day!" "Starting ludusavi backup..."
+        ${pkgs.ludusavi}/bin/ludusavi backup --force || ${pkgs.libnotify}/bin/notify-send "Ludusavi backup failed" "Check system status for more information."
+      ''}";
     };
   };
 
