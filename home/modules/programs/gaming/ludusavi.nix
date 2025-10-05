1840df1ba8166c903e38b88acc37828a912a00fc
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-05 14:55:15 -0400

    add auto backups ludusavi

--- home/modules/programs/gaming/ludusavi.nix
+++ home/modules/programs/gaming/ludusavi.nix
@@ -2,4 +2,29 @@
   home.packages = with pkgs; [
     ludusavi
   ];
+
+  # Automate backups: https://github.com/mtkennerly/ludusavi/blob/master/docs/help/backup-automation.md
+  systemd.user.services.ludusavi = {
+    Unit = {
+      Description = "Ludusavi backup";
+    };
+
+    Service = {
+      ExecStart = "${pkgs.ludusavi}/bin/ludusavi backup --force";
+    };
+  };
+
+  systemd.user.timers.ludusavi = {
+    Unit = {
+      Description = "Ludusavi backup timer";
+    };
+
+    Timer = {
+      OnCalendar = "16:00";
+    };
+
+    Install = {
+      WantedBy = ["timers.target"];
+    };
+  };
 }
