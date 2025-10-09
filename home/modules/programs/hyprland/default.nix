e4c579eb407ff5f94a57ddd6ecd624b4a1252507
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-08 21:56:19 -0400

    replace open_url script w nix bash script

--- home/modules/programs/hyprland/default.nix
+++ home/modules/programs/hyprland/default.nix
@@ -27,8 +27,6 @@
 
     # Network manager
     networkmanagerapplet
-
-    jq
   ];
 
   # Without this, home manager can't symlink files to .config (https://github.com/nix-community/home-manager/issues/1807#issuecomment-3131623755)
@@ -47,16 +45,37 @@
   };
 
   # Workaround to open hyprlinks in current workspace (just like the one here!): https://www.reddit.com/r/hyprland/comments/1b5jvvm/opening_browser_in_the_current_workspace/
-  # First add the script to path
-  home.sessionPath = [
-    "$HOME/bin/open_url.sh"
-  ];
-
   xdg = {
     # The script to run every time a url is clicked
     desktopEntries."open_url" = {
       name = "Open URL";
-      exec = "open_url.sh %u";
+      exec = "${pkgs.writeShellScript "open-url" ''
+        #!/run/current-system/sw/bin/bash
+        # Get current workspace ID
+        workspace_id=$(${pkgs.hyprland}/bin/hyprctl activeworkspace -j | ${pkgs.jq}/bin/jq ".id")
+
+        # Get the address of a Firefox window on the current workspace, if any
+        current_ff_address=$(${pkgs.hyprland}/bin/hyprctl clients -j | ${pkgs.jq}/bin/jq -r ".[] | select(.initialClass == \"firefox-nightly\" and .workspace.id == $workspace_id) | .address")
+
+        # Get the URL from the first argument
+        url="$1"
+
+        # Check if a URL was provided
+        if [ -z "$url" ]; then
+            echo "No URL provided" >&2
+            exit 1
+        fi
+
+        if [ -n "$current_ff_address" ]; then
+            # Focus the existing Firefox window
+            ${pkgs.hyprland}/bin/hyprctl dispatch focuswindow address:"$current_ff_address"
+            # Open the URL in a new tab
+            firefox-nightly --new-tab "$url" &
+        else
+            # No Firefox window on this workspace, open a new window
+            firefox-nightly --new-window "$url" &
+        fi
+      ''} %u";
       terminal = false;
       type = "Application";
 
