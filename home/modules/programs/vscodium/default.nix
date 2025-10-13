daeec0a360df8a60b37288a9a634b3f9291bc091
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-12 21:18:18 -0400

    add vscodium fhs

--- home/modules/programs/vscodium/default.nix
+++ home/modules/programs/vscodium/default.nix
@@ -7,7 +7,8 @@
 
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium;
+    # Use vscodium fhs so lldb works: https://www.reddit.com/r/NixOS/comments/1e724mx/how_can_i_debug_rust_with_vscodium_and/
+    package = pkgs.vscodium.fhs;
   };
 
   # Run vscodium under wayland
