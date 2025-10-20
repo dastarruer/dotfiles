33fb94610217d9523c80540314d1f149685727de
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-20 13:17:57 -0400

    add wine runner settings for lutris

--- home/modules/programs/gaming/lutris.nix
+++ home/modules/programs/gaming/lutris.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs.lutris = {
     enable = true;
 
@@ -11,5 +15,21 @@
       gamescope
       gamemode
     ];
+
+    runners.wine.settings.system = {
+      # Enable gamescope. Pretty useful since it has a frame limiter, and
+      # automatically adjusts the game resolution to meet said frame rate.
+      gamescope = "true";
+      gamescope_flags = lib.concatStringsSep " " [
+        "--force-grab-cursor" # Forces the game to capture mouse movement, so camera movement is not restricted
+        "--expose-wayland" # Wayland support
+      ];
+
+      # Set fps limit of 30
+      gamescope_fps_limiter = "30";
+
+      # Fullscreen game window
+      gamescope_window_mode = "-f";
+    };
   };
 }
