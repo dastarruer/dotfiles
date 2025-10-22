78d8e22c9eb24743f70cedbaba59d473da5e187f
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-10-22 14:31:49 -0400

    stuff

--- nix/modules/audio.nix
+++ nix/modules/audio.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   # From https://wiki.nixos.org/wiki/PipeWire
+  # having issues with your headset again? try this!!! https://wiki.archlinux.org/title/Bluetooth_headset#Switch_between_HSP/HFP_and_A2DP_setting
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true; # if not already enabled
