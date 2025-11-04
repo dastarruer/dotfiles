59815f39f18ea6c63cd5a8589969d4008bfae5ac
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-11-03 21:57:45 -0400

    Add quizlet anki addon

--- home/modules/programs/anki/addons/default.nix
+++ home/modules/programs/anki/addons/default.nix
@@ -3,12 +3,12 @@
     ./image-occlusion-enhanced.nix
     ./passfail2.nix
     ./recolor.nix
-    ./anki-quizlet-importer-extended.nix
     # ./auto-sync.nix
   ];
 
   # install addons that don't need configuration
   programs.anki.addons = with pkgs.ankiAddons; [
     review-heatmap
+    anki-quizlet-importer-extended
   ];
 }
