a5d083133d8f9312e0e7972d72cc69be302427e6
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-11-06 21:59:03 -0400

    stuff

--- home/modules/programs/waybar/colors.nix
+++ home/modules/programs/waybar/colors.nix
@@ -25,7 +25,9 @@
   #   @define-color color15 #${config.lib.stylix.colors.base0F};
   # '';
 
-  home.file.".cache/waybar/colors.css".text = ''
+  # home.file.".cache/waybar/colors.css".text = ''
+  # '';
+  programs.waybar.style = ''
     /* ================================ */
     /*            Stylix Colors         */
     /* ================================ */
@@ -33,5 +35,63 @@
     @define-color fg #${config.lib.stylix.colors.base05};
     @define-color bg #${config.lib.stylix.colors.base00};
     @define-color bg-alt #${config.lib.stylix.colors.base01};
+
+    * {
+        font-size: 15px;
+        min-height: 0;
+    }
+
+    window#waybar {
+        background-color: @bg;
+        border-bottom: 0px solid @bg-alt;
+    }
+
+    #mode {
+        font-family: "JetBrainsMono Nerd Font";
+        font-weight: bold;
+        color: @accent;
+    }
+
+    #language,
+    #pulseaudio,
+    #battery,
+    #backlight,
+    #custom-clock,
+    #cpu {
+        font-family: "JetBrainsMono Nerd Font";
+        color: @fg;
+    }
+
+    #workspaces {
+        font-family: "JetBrainsMono Nerd Font";
+        border-bottom: 1px solid @bg-alt;
+    }
+
+    #workspaces button {
+        padding: 7px 12px 7px 12px;
+        color: @fg;
+        background-color: @bg;
+        border: none;
+    }
+
+    #workspaces button:hover {
+        background: none;
+        border: none;
+        border-color: transparent;
+        transition: none;
+    }
+
+    #workspaces button.focused {
+        border-radius: 0;
+        color: @accent;
+        font-weight: bold;
+    }
+
+    #tray {
+        font-family: "Ubuntu";
+        padding-right: 5px;
+        padding-left: 10px;
+    }
+
   '';
 }
--- home/modules/programs/waybar/default.nix
+++ home/modules/programs/waybar/default.nix
@@ -4,7 +4,74 @@
     ./colors.nix
   ];
 
-  programs.waybar.enable = true;
+  programs.waybar = {
+    enable = true;
+
+    settings = {
+      config = {
+        layer = "bottom";
+        position = "top";
+        height = 30;
+        spacing = 0;
+
+        "modules-left" = ["hyprland/workspaces" "hyprland/mode" "cpu"];
+        "modules-center" = ["custom/clock"];
+        "modules-right" = ["hyprland/language" "pulseaudio" "battery" "backlight" "tray"];
+
+        cpu = {
+          interval = 10;
+          format = "<span color='#${config.lib.stylix.colors.base01}'> / </span><span color='#${config.lib.stylix.colors.base09}'><b>cpu:</b></span> {}%<span color='#${config.lib.stylix.colors.base01}'> ]</span>";
+        };
+
+        "hyprland/workspaces" = {
+          "disable-scroll" = true;
+          format = "{icon}{name}";
+          "format-icons" = {
+            default = " ";
+            urgent = "!";
+            active = "*";
+          };
+        };
+
+        "hyprland/mode" = {
+          format = "  {}";
+        };
+
+        "custom/clock" = {
+          format = "<span color='#${config.lib.stylix.colors.base01}'>[ </span>{text}<span color='#${config.lib.stylix.colors.base01}'> ]</span>";
+          exec = "date +'%I:%M %A, %b %d' | awk '{print tolower($0)}'";
+          interval = 3;
+        };
+
+        "hyprland/language" = {
+          format = "<span color='#${config.lib.stylix.colors.base01}'>[ </span><span color='#${config.lib.stylix.colors.base09}'><b>key: </b></span>{short}<span color='#${config.lib.stylix.colors.base01}'> / </span>";
+          tooltip = false;
+        };
+
+        pulseaudio = {
+          format = "<span color='#${config.lib.stylix.colors.base09}'><b>vol:</b></span> {volume}%";
+        };
+
+        battery = {
+          format = "<span color='#${config.lib.stylix.colors.base01}'> / </span><span color='#${config.lib.stylix.colors.base09}'><b>bat:</b></span> {capacity}%";
+          "format-charging" = "<span color='#${config.lib.stylix.colors.base01}'> / </span><span color='#${config.lib.stylix.colors.base09}'><b>ac:</b></span> {capacity}%";
+          tooltip = false;
+          interval = 20;
+        };
+
+        backlight = {
+          format = "<span color='#${config.lib.stylix.colors.base01}'> / </span><span color='#${config.lib.stylix.colors.base09}'><b>mon:</b></span> {percent}%<span color='#${config.lib.stylix.colors.base01}'> ]</span>";
+          tooltip = false;
+        };
+
+        tray = {
+          "icon-size" = 20;
+          spacing = 7;
+          tooltip = false;
+        };
+      };
+    };
+  };
 
   # Symlink the entire Waybar directory
   home.file.".config/waybar" = {
