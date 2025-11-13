d30e75d790fc6c59247046b0b63826b0f6c26f8f
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-11-12 20:36:03 -0400

    stuff

--- home/modules/programs/zathura.nix
+++ home/modules/programs/zathura.nix
@@ -3,6 +3,13 @@
     enable = true;
 
     options = {
+      # Set dark mode by default
+      recolor = true;
+
+      # Fill window upon opening
+      adjust_window = "best-fit";
+      adjust_open = "best-fit";
+
       window-title-home-tilde = true;
       statusbar-basename = true;
       selection-clipboard = "clipboard";
