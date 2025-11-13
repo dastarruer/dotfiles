d76ebdc6bb181a2e35db37f6cb7d1f34767fb3a9
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-11-12 22:32:54 -0400

    fix alert.sh

--- config/dunst/scripts/alert.sh
+++ config/dunst/scripts/alert.sh
@@ -3,5 +3,8 @@
 if [ "$DUNST_APP_NAME" != "Spotify" ] && \
    [ "$DUNST_APP_NAME" != "flameshot" ] && \
    [[ "$DUNST_SUMMARY" != *"Wallpaper set to:"* ]]; then
-    pw-play "$HOME/.config/dunst/alerts/default.wav"
+    # Only play if no pw-play process is currently running
+    if ! pgrep -x pw-play >/dev/null; then
+        pw-play "$HOME/.config/dunst/alerts/default.wav" &
+    fi
 fi
