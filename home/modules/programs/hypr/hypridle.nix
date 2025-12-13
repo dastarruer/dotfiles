0522dd82ceed5c6adfb987cfcf1544aaffa1ae09
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-12-13 12:38:56 -0400

    rewrite hypridle

--- home/modules/programs/hypr/hypridle.nix
+++ home/modules/programs/hypr/hypridle.nix
@@ -1,10 +1,29 @@
-{pkgs, ...}: {
+{...}: {
   services.hypridle = {
     enable = true;
+
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "hyprlock";
+      };
+
+      listener = [
+        {
+          timeout = 180;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+        {
+          timeout = 1800;
+          on-timeout = "systemctl suspend || loginctl suspend";
+        }
+      ];
+    };
   };
 
   # Idk why but services.hypridle doesnt work
-  home.packages = with pkgs; [
-    hypridle
-  ];
+  # home.packages = with pkgs; [
+  #   hypridle
+  # ];
 }
