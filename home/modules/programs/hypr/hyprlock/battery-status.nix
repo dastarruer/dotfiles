bd5d7d03528a665d3ea2cedf12e2c2a84f01a80a
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-12-13 11:18:53 -0400

    rewrite battery_status script in nix

--- home/modules/programs/hypr/hyprlock/battery-status.nix
+++ home/modules/programs/hypr/hyprlock/battery-status.nix
@@ -1,9 +1,35 @@
-{...}: {
+{pkgs, ...}: {
   programs.hyprlock.settings.label = [
     {
       monitor = "";
-      text = ''
-        cmd[update:1000] echo "$(${./scripts/battery-status.sh})"
+      text = let
+        name = "battery_status";
+      in ''
+        cmd[update:1000] echo "$(${
+          pkgs.writeShellApplication {
+            name = "${name}";
+            text = ''
+              status="$(cat /sys/class/power_supply/BAT0/status)"
+              level="$(cat /sys/class/power_supply/BAT0/capacity)"
+
+              if [[ "$status" == "Discharging" || "$status" == "Full" ]]; then
+                if [[ "$level" -eq 0 ]]; then
+                  echo "$level% "
+                elif [[ "$level" -ge 1 && "$level" -le 25 ]]; then
+                  echo "$level% "
+                elif [[ "$level" -ge 26 && "$level" -le 50 ]]; then
+                  echo "$level% "
+                elif [[ "$level" -ge 51 && "$level" -le 75 ]]; then
+                  echo "$level% "
+                elif [[ "$level" -ge 76 && "$level" -le 100 ]]; then
+                  echo "$level% "
+                fi
+              elif [[ "$status" == "Charging" ]]; then
+                echo "$level% "
+              fi
+            '';
+          }
+        }/bin/${name})"
       '';
       color = "rgba(242, 243, 244, 0.75)";
       font_size = 15;
