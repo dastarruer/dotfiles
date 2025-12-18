ebe6ad6cc907de40f619770b1800cc4302b43e77
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-12-18 12:41:03 +0530

    move open_url to hyprland

--- home/modules/programs/hypr/hyprland/default.nix
+++ home/modules/programs/hypr/hyprland/default.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   pkgs,
+  config,
   ...
 }: {
   imports = [
@@ -25,8 +26,72 @@
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
-  home = {
-    # Use wayland
-    sessionVariables.NIXOS_OZONE_WL = "1";
+  # Use wayland for chromium/electron apps
+  home.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # Workaround to open hyprlinks in current workspace (just like the one here!): https://www.reddit.com/r/hyprland/comments/1b5jvvm/opening_browser_in_the_current_workspace/
+  xdg = {
+    # The script to run every time a url is clicked
+    desktopEntries."open_url" = {
+      name = "Open URL";
+      exec = "${pkgs.writeShellApplication {
+        name = "open-url";
+
+        runtimeInputs = with pkgs; let
+          firefoxPkg = config.programs.firefox.package;
+        in [
+          jq
+          hyprland
+          firefoxPkg
+        ];
+
+        text = ''
+          #!/run/current-system/sw/bin/bash
+          # Get current workspace ID
+          workspace_id=$(hyprctl activeworkspace -j | jq ".id")
+
+          # Get the address of a Firefox window on the current workspace, if any
+          current_ff_address=$(hyprctl clients -j | jq -r ".[] | select(.initialClass == \"firefox-nightly\" and .workspace.id == $workspace_id) | .address")
+
+          # Get the URL from the first argument
+          url="$1"
+
+          # Check if a URL was provided
+          if [ -z "$url" ]; then
+              echo "No URL provided" >&2
+              exit 1
+          fi
+
+          if [ -n "$current_ff_address" ]; then
+              # Focus the existing Firefox window
+              hyprctl dispatch focuswindow address:"$current_ff_address"
+              # Open the URL in a new tab
+              firefox-nightly --new-tab "$url" &
+          else
+              # No Firefox window on this workspace, open a new window
+              firefox-nightly --new-window "$url" &
+          fi
+        '';
+      }}/bin/open-url %u";
+      terminal = false;
+      type = "Application";
+
+      # Don't show this in rofi
+      noDisplay = true;
+    };
+
+    # This section just runs the previously declared .desktop file when a link is clicked
+    mimeApps = {
+      enable = true;
+      associations.added = {
+        "x-scheme-handler/http" = ["open_url.desktop"];
+        "x-scheme-handler/https" = ["open_url.desktop"];
+      };
+
+      defaultApplications = {
+        "x-scheme-handler/http" = ["open_url.desktop"];
+        "x-scheme-handler/https" = ["open_url.desktop"];
+      };
+    };
   };
 }
