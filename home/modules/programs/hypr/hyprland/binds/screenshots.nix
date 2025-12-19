8c02b499ac2d31f766b9c917c208ac083cb8aefd
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-12-19 12:51:00 +0530

    stuff

--- home/modules/programs/hypr/hyprland/binds/screenshots.nix
+++ home/modules/programs/hypr/hyprland/binds/screenshots.nix
@@ -14,6 +14,7 @@
       "SUPER+SHIFT, P, exec, ${pkgs.grimblast}/bin/grimblast copysave area -n"
       "SUPER+CTRL, P, exec, ${pkgs.grimblast}/bin/grimblast copysave screen -n"
       ''SUPER+SHIFT, O, exec, ${pkgs.grimblast}/bin/grimblast save area - | ${pkgs.tesseract}/bin/tesseract stdin stdout | ${pkgs.wl-clipboard}/bin/wl-copy && notify-send "Clipboard:" "$(${pkgs.wl-clipboard}/bin/wl-paste)"''
+      "SUPER, M, exec, ${pkgs.wl-clipboard}/bin/wl-paste | ${pkgs.swappy}/bin/swappy -f -"
     ];
   };
 }
