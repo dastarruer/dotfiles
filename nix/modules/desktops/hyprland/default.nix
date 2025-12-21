aa6f8598a0e51ecb6bcc85d1ffeff48fd3770596
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-12-21 09:42:17 +0530

    use cachix for hyprland

--- nix/modules/desktops/hyprland/default.nix
+++ nix/modules/desktops/hyprland/default.nix
@@ -1,6 +1,13 @@
 {pkgs, ...}: {
   programs.uwsm.enable = true;
 
+  # Reduce rebuild times by using prebuilt hyprland binaries rather than building from source (https://wiki.hypr.land/Nix/Cachix/)
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
