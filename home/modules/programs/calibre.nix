104f0d8ed16e896038035a24dc99375df1551d3f
Ayush Pramanik <ayushpramanik399@gmail.com>
2025-12-22 09:59:30 +0530

    add calibre theme

--- home/modules/programs/calibre.nix
+++ home/modules/programs/calibre.nix
@@ -1,3 +1,64 @@
-{...}: {
-  services.calibre.enable = true;
+{config, ...}: {
+  programs.calibre.enable = true;
+
+  # Style guide here: https://github.com/chriskempson/base16/blob/main/styling.md
+  # Has to be applied manually with: Preferences -> Look and Feel -> Adjust colors -> Import
+  home.file.".config/calibre/theme.calibre-palette".text = ''
+    {
+      "dark": {
+        "palette": {
+          "AlternateBase": "#${config.lib.stylix.colors.base00}",
+          "Base": "#${config.lib.stylix.colors.base01}",
+          "BrightText": "#${config.lib.stylix.colors.base01}",
+          "BrightText-disabled": "#${config.lib.stylix.colors.base01}",
+          "Button": "#${config.lib.stylix.colors.base00}",
+          "ButtonText": "#${config.lib.stylix.colors.base05}",
+          "ButtonText-disabled": "#${config.lib.stylix.colors.base04}",
+          "Highlight": "#${config.lib.stylix.colors.base09}",
+          "HighlightedText": "#${config.lib.stylix.colors.base01}",
+          "HighlightedText-disabled": "#${config.lib.stylix.colors.base01}",
+          "Link": "#${config.lib.stylix.colors.base08}",
+          "LinkVisited": "#${config.lib.stylix.colors.base0E}",
+          "PlaceholderText": "#${config.lib.stylix.colors.base05}",
+          "PlaceholderText-disabled": "#${config.lib.stylix.colors.base05}",
+          "Text": "#${config.lib.stylix.colors.base05}",
+          "Text-disabled": "#${config.lib.stylix.colors.base04}",
+          "ToolTipBase": "#${config.lib.stylix.colors.base00}",
+          "ToolTipText": "#${config.lib.stylix.colors.base05}",
+          "ToolTipText-disabled": "#${config.lib.stylix.colors.base05}",
+          "Window": "#${config.lib.stylix.colors.base00}",
+          "WindowText": "#${config.lib.stylix.colors.base05}",
+          "WindowText-disabled": "#${config.lib.stylix.colors.base04}"
+        },
+        "use_custom": true
+      },
+      "light": {
+        "palette": {
+          "AlternateBase": "#${config.lib.stylix.colors.base00}",
+          "Base": "#${config.lib.stylix.colors.base01}",
+          "BrightText": "#${config.lib.stylix.colors.base01}",
+          "BrightText-disabled": "#${config.lib.stylix.colors.base01}",
+          "Button": "#${config.lib.stylix.colors.base00}",
+          "ButtonText": "#${config.lib.stylix.colors.base05}",
+          "ButtonText-disabled": "#${config.lib.stylix.colors.base04}",
+          "Highlight": "#${config.lib.stylix.colors.base09}",
+          "HighlightedText": "#${config.lib.stylix.colors.base01}",
+          "HighlightedText-disabled": "#${config.lib.stylix.colors.base01}",
+          "Link": "#${config.lib.stylix.colors.base08}",
+          "LinkVisited": "#${config.lib.stylix.colors.base0E}",
+          "PlaceholderText": "#${config.lib.stylix.colors.base05}",
+          "PlaceholderText-disabled": "#${config.lib.stylix.colors.base05}",
+          "Text": "#${config.lib.stylix.colors.base05}",
+          "Text-disabled": "#${config.lib.stylix.colors.base04}",
+          "ToolTipBase": "#${config.lib.stylix.colors.base00}",
+          "ToolTipText": "#${config.lib.stylix.colors.base05}",
+          "ToolTipText-disabled": "#${config.lib.stylix.colors.base05}",
+          "Window": "#${config.lib.stylix.colors.base00}",
+          "WindowText": "#${config.lib.stylix.colors.base05}",
+          "WindowText-disabled": "#${config.lib.stylix.colors.base04}"
+        },
+        "use_custom": false
+      }
+    }
+  '';
 }
