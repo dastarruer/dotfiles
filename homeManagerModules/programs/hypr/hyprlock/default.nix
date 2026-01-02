c332b8e26ec52ef05a0cc805ba0c02e1d5d32ccf
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-01-02 13:27:10 +0530

    add option for hyprlock

--- homeManagerModules/programs/hypr/hyprlock/default.nix
+++ homeManagerModules/programs/hypr/hyprlock/default.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   imports = [
     ./pfp.nix
     ./password-input.nix
@@ -7,28 +12,38 @@
     ./background.nix
   ];
 
-  # Config taken from here: https://github.com/FireDrop6000/hyprland-mydots/blob/master/.config/hypr/hyprlock.conf
-  programs.hyprlock = {
-    enable = true;
+  options = {
+    dotfiles.hypr.hyprlock.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable hyprlock.";
+    };
   };
 
-  # https://wiki.archlinux.org/title/Power_management/Suspend_and_hibernate#Custom_systemd_units
-  systemd.user.services.hyprlock = {
-    Unit = {
-      Description = "Lock the screen before sleep";
-      Before = ["sleep.target"];
+  config = lib.mkIf config.dotfiles.hypr.hyprlock.enable {
+    # Config taken from here: https://github.com/FireDrop6000/hyprland-mydots/blob/master/.config/hypr/hyprlock.conf
+    programs.hyprlock = {
+      enable = true;
     };
 
-    Install = {
-      WantedBy = ["sleep.target"];
-    };
+    # https://wiki.archlinux.org/title/Power_management/Suspend_and_hibernate#Custom_systemd_units
+    systemd.user.services.hyprlock = {
+      Unit = {
+        Description = "Lock the screen before sleep";
+        Before = ["sleep.target"];
+      };
 
-    Service = {
-      Type = "forking";
-      User = "%I";
-      Environment = "DISPLAY=:0";
-      ExecStart = "${pkgs.hyprlock}/bin/hyprlock";
-      ExecStartPost = "${pkgs.coreutils}/bin/sleep 1";
+      Install = {
+        WantedBy = ["sleep.target"];
+      };
+
+      Service = {
+        Type = "forking";
+        User = "%I";
+        Environment = "DISPLAY=:0";
+        ExecStart = "${pkgs.hyprlock}/bin/hyprlock";
+        ExecStartPost = "${pkgs.coreutils}/bin/sleep 1";
+      };
     };
   };
 }
