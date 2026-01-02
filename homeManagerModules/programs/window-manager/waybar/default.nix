2b7d7a5bd0435cdd45c1a766f8d579e49ade0a91
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-01-02 15:18:52 +0530

    add waybar module

--- homeManagerModules/programs/window-manager/waybar/default.nix
+++ homeManagerModules/programs/window-manager/waybar/default.nix
@@ -1,25 +1,39 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   imports = [
     ./theme.nix
     ./config.nix
   ];
 
-  programs.waybar.enable = true;
+  options = {
+    dotfiles.window-manager.waybar.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable waybar, a status bar.";
+    };
+  };
 
-  wayland.windowManager.hyprland.settings = {
-    # Start waybar
-    "exec-once" = [
-      "waybar &"
-    ];
-    # Start/stop waybar keybind
-    bind = [
-      "SUPER, W, exec, pidof waybar && pkill waybar || waybar &"
-    ];
-    # more hyprland stuff i dont understand
-    layerrule = [
-      "blur, waybar"
-      "xray 0, waybar"
-      "ignorezero, waybar"
-    ];
+  config = lib.mkIf config.dotfiles.window-manager.waybar.enable {
+    programs.waybar.enable = true;
+
+    wayland.windowManager.hyprland.settings = {
+      # Start waybar
+      "exec-once" = [
+        "waybar &"
+      ];
+      # Start/stop waybar keybind
+      bind = [
+        "SUPER, W, exec, pidof waybar && pkill waybar || waybar &"
+      ];
+      # more hyprland stuff i dont understand
+      layerrule = [
+        "blur, waybar"
+        "xray 0, waybar"
+        "ignorezero, waybar"
+      ];
+    };
   };
 }
