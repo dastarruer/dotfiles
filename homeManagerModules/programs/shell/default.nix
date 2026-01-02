c5d5b63f0d0a7e6270c3d10bf625296ccc2dc221
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-01-02 13:20:07 +0530

    add shell.enable

--- homeManagerModules/programs/shell/default.nix
+++ homeManagerModules/programs/shell/default.nix
@@ -1,11 +1,23 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   imports = [
     ./fish.nix
     ./starship.nix
   ];
 
+  options = {
+    myPrograms.shell.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable the fish shell and starship.";
+    };
+  };
+
   myPrograms.shell = {
-    fish.enable = true;
-    starship.enable = true;
+    fish.enable = lib.mkDefault config.myPrograms.shell.enable;
+    starship.enable = lib.mkDefault config.myPrograms.shell.enable;
   };
 }
