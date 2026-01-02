2ec7d8a7d9aec0de0c762c86ce8bac4a8ee16add
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-01-02 14:35:46 +0530

    add option for spicetify

--- homeManagerModules/programs/spicetify/default.nix
+++ homeManagerModules/programs/spicetify/default.nix
@@ -1,35 +1,50 @@
 # HELLO FUTURE ME basically spicetify on nix will not work if you also install spotify in packages.nix so yeah
 # https://gerg-l.github.io/spicetify-nix/usage.html
-{spicePkgs, ...}: {
+{
+  config,
+  lib,
+  spicePkgs,
+  ...
+}: {
   imports = [
     ./extensions.nix
     ./snippets.nix
     ./theme.nix
   ];
 
-  programs.spicetify = {
-    enable = true;
-
-    # Custom apps
-    enabledCustomApps = with spicePkgs.apps; [
-      marketplace
-    ];
-
-    # Chromium devtools
-    alwaysEnableDevTools = true;
-
-    wayland = true;
+  options = {
+    dotfiles.spicetify.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable spicetify, a modified version of Spotify.";
+    };
   };
 
-  wayland.windowManager.hyprland.settings = {
-    windowrule = [
-      "float, class:^(spotify)$"
-      "move 81 146, class:^(spotify)$"
-      "size 1068 670, class:^(spotify)$"
-    ];
+  config = lib.mkIf config.dotfiles.spicetify.enable {
+    programs.spicetify = {
+      enable = true;
 
-    windowrulev2 = [
-      "workspace 5 silent,class:^(spotify)$"
-    ];
+      # Custom apps
+      enabledCustomApps = with spicePkgs.apps; [
+        marketplace
+      ];
+
+      # Chromium devtools
+      alwaysEnableDevTools = true;
+
+      wayland = true;
+    };
+
+    wayland.windowManager.hyprland.settings = {
+      windowrule = [
+        "float, class:^(spotify)$"
+        "move 81 146, class:^(spotify)$"
+        "size 1068 670, class:^(spotify)$"
+      ];
+
+      windowrulev2 = [
+        "workspace 5 silent,class:^(spotify)$"
+      ];
+    };
   };
 }
