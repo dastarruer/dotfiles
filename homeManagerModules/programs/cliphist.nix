a0e4696fc9476335e35aae925cc099a289c1dc0b
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-01-02 15:46:18 +0530

    add cliphist module

--- homeManagerModules/programs/cliphist.nix
+++ homeManagerModules/programs/cliphist.nix
@@ -1,26 +1,37 @@
 {
-  pkgs,
-  lib,
   config,
+  lib,
+  pkgs,
   ...
 }: let
   hyprland = config.dotfiles.window-manager.hypr.hyprland;
   rofi = config.dotfiles.window-manager.rofi;
+  cfg = config.dotfiles.cliphist;
 in {
-  home.packages = with pkgs; [
-    cliphist
-  ];
+  options = {
+    dotfiles.cliphist.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable cliphist, a clipboard history manager.";
+    };
+  };
 
-  wayland.windowManager.hyprland.settings = lib.mkIf hyprland.enable {
-    # Start clipboard manager on startup
-    "exec-once" = [
-      "wl-paste --type text --watch cliphist store &"
-      "wl-paste --type image --watch cliphist store &"
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      cliphist
     ];
 
-    # If rofi is enabled
-    bind = lib.mkIf rofi.enable [
-      "SUPER, V, exec, cliphist list | rofi -dmenu | cliphist decode | wl-copy"
-    ];
+    wayland.windowManager.hyprland.settings = lib.mkIf hyprland.enable {
+      # Start clipboard manager on startup
+      "exec-once" = [
+        "wl-paste --type text --watch cliphist store &"
+        "wl-paste --type image --watch cliphist store &"
+      ];
+
+      # If rofi is enabled
+      bind = lib.mkIf rofi.enable [
+        "SUPER, V, exec, cliphist list | rofi -dmenu | cliphist decode | wl-copy"
+      ];
+    };
   };
 }
