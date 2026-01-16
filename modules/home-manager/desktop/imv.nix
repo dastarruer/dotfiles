9a989aecc2a5946a016a3dd2291a72572482f593
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-01-16 18:14:41 -0400

    make imv the default image viewer

--- modules/home-manager/desktop/imv.nix
+++ modules/home-manager/desktop/imv.nix
@@ -2,7 +2,30 @@
   config,
   lib,
   ...
-}: {
+}: let
+  # Define the list of image mimetypes to associate with imv
+  imageMimes = [
+    "image/bmp"
+    "image/gif"
+    "image/jpeg"
+    "image/jpg"
+    "image/pjpeg"
+    "image/png"
+    "image/tiff"
+    "image/x-bmp"
+    "image/x-pcx"
+    "image/x-png"
+    "image/x-portable-anymap"
+    "image/x-portable-bitmap"
+    "image/x-portable-graymap"
+    "image/x-portable-pixmap"
+    "image/x-tga"
+    "image/x-xbitmap"
+    "image/heic"
+    "image/avif"
+    "image/webp"
+  ];
+in {
   options = {
     home-manager.desktop.imv.enable = lib.mkOption {
       type = lib.types.bool;
@@ -20,5 +43,9 @@
         aliases.r = "rotate by 90";
       };
     };
+
+    # Set as default for all common image types
+    xdg.mimeApps.defaultApplications =
+      lib.genAttrs imageMimes (_: "imv.desktop");
   };
 }
