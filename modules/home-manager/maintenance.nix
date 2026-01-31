ad8d9b0ce1c5d44ce58fd6dc5ce025d640195dbc
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-01-30 21:09:52 -0400

    Remove auto upgrade

--- modules/home-manager/maintenance.nix
+++ modules/home-manager/maintenance.nix
@@ -4,45 +4,45 @@
   pkgs,
   ...
 }: {
-  services.home-manager.autoUpgrade = {
-    enable = true;
-    frequency = "Sat 12:00";
-    useFlake = true;
-    # inputs.self.outDir will throw an error when used with useFlake for whatever reason.
-    flakeDir = "${config.home.homeDirectory}/.dotfiles";
-  };
+  # services.home-manager.autoUpgrade = {
+  #   enable = true;
+  #   frequency = "Sat 12:00";
+  #   useFlake = true;
+  #   # inputs.self.outDir will throw an error when used with useFlake for whatever reason.
+  #   flakeDir = "${config.home.homeDirectory}/.dotfiles";
+  # };
 
-  systemd.user.services.push-flake = {
-    Unit = {
-      Description = "Push home-manager flake changes to remote repository";
+  # systemd.user.services.push-flake = {
+  #   Unit = {
+  #     Description = "Push home-manager flake changes to remote repository";
 
-      After = ["home-manager-auto-upgrade.service"];
-      Requires = ["home-manager-auto-upgrade.service"];
-    };
+  #     After = ["home-manager-auto-upgrade.service"];
+  #     Requires = ["home-manager-auto-upgrade.service"];
+  #   };
 
-    Service = {
-      ExecStart = let
-        name = "push_flake";
-      in "${pkgs.writeShellApplication {
-        name = name;
+  #   Service = {
+  #     ExecStart = let
+  #       name = "push_flake";
+  #     in "${pkgs.writeShellApplication {
+  #       name = name;
 
-        runtimeInputs = with pkgs; [
-          git
-        ];
+  #       runtimeInputs = with pkgs; [
+  #         git
+  #       ];
 
-        text = ''
-          cd "${config.home.homeDirectory}/.dotfiles"
-          git add .
-          git commit -m "update flake" || exit 0
-          git push
-        '';
-      }}/bin/${name}";
+  #       text = ''
+  #         cd "${config.home.homeDirectory}/.dotfiles"
+  #         git add .
+  #         git commit -m "update flake" || exit 0
+  #         git push
+  #       '';
+  #     }}/bin/${name}";
 
-      Restart = "on-failure";
-      RestartSec = 30;
-      Type = "oneshot";
-    };
-  };
+  #     Restart = "on-failure";
+  #     RestartSec = 30;
+  #     Type = "oneshot";
+  #   };
+  # };
 
   # Clean nix store and remove old home-manager generations
   programs.nh.clean = {
