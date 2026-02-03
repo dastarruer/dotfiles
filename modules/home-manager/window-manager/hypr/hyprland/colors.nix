af7beedae6b3f009805971be669f2f3fb54a28e3
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-02-03 16:49:31 -0400

    use accent color for active window

--- modules/home-manager/window-manager/hypr/hyprland/colors.nix
+++ modules/home-manager/window-manager/hypr/hyprland/colors.nix
@@ -1,7 +1,8 @@
 {config, ...}: {
   wayland.windowManager.hyprland.settings = {
     general = {
-      "col.active_border" = "rgb(${config.lib.stylix.colors.base05})";
+      "col.active_border" = "rgb(${config.home-manager.theme.accent})";
+      # Make inactive border transparent
       "col.inactive_border" = "rgba(00000000)"; #"rgb(${config.lib.stylix.colors.base03})";
     };
 
