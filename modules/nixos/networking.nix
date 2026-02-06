f76566aaecb0a8fa7a955005a83ad05f40d3a31e
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-02-06 14:38:42 -0400

    Add extra dns settings

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -47,7 +47,10 @@
     # Enable iptables firewall https://nixos.wiki/wiki/Firewall
     firewall.enable = true;
 
-    # DNS servers
+    # DNS settings (https://wiki.nixos.org/wiki/NetworkManager#DNS_Management)
+    networkmanager.dns = "none";
+    useDHCP = false;
+    dhcpcd.enable = false;
     nameservers = [
       "1.1.1.1" # Cloudflare (fast + privacy focused)
       "9.9.9.9" # Quad9 (blocks malicious domains)
