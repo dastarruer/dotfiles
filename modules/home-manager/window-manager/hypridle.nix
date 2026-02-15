449a5909dd4bb7d9bde001d62b274cba09a29ee8
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-02-15 10:41:27 -0400

    move hypridle sleep command to hyprlock

--- modules/home-manager/window-manager/hypridle.nix
+++ modules/home-manager/window-manager/hypridle.nix
@@ -5,7 +5,6 @@
   ...
 }: let
   cfg = config.home-manager.window-manager.hypridle;
-  hyprlock = config.home-manager.window-manager.hyprlock;
   hyprland = config.home-manager.window-manager.hyprland;
 in {
   options = {
@@ -20,12 +19,8 @@ in {
     services.hypridle = {
       enable = true;
 
-      # These settings will lock the screen before suspend
       settings.general = {
-        lock_cmd =
-          lib.mkIf hyprlock.enable
-          "pidof hyprlock || ${pkgs.hyprlock}/bin/hyprlock && sleep 0.1";
-
+        # Pause all players and lock screen
         before_sleep_cmd = "${lib.getExe pkgs.pause-all} && ${pkgs.systemd}/bin/loginctl lock-session";
         after_sleep_cmd =
           lib.mkIf hyprland.enable
