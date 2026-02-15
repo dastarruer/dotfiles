d5bd40793d3c5593bc509577262dfd79f46312b0
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-02-15 19:53:20 -0400

    remove dunst stuff

--- modules/home-manager/window-manager/dunst/default.nix
+++ modules/home-manager/window-manager/dunst/default.nix
@@ -16,82 +16,82 @@
     services.dunst = {
       enable = true;
 
-      iconTheme = {
-        # Dunst will share the same icon theme as gtk
-        package = config.gtk.iconTheme.package;
-        name = "${config.gtk.iconTheme.name}";
-        size = "96x96";
-      };
+      # iconTheme = {
+      #   # Dunst will share the same icon theme as gtk
+      #   package = config.gtk.iconTheme.package;
+      #   name = "${config.gtk.iconTheme.name}";
+      #   size = "96x96";
+      # };
 
       # https://github.com/martin-djakovic/dotfiles/blob/main/.config/dunst/dunstrc
       settings = {
-        global = {
-          monitor = 0;
-          follow = "none";
-          width = 380;
-          height = 200;
-          origin = "top-right";
-          offset = "5x5";
-          scale = 0;
-          notification_limit = 20;
+        #   global = {
+        #     monitor = 0;
+        #     follow = "none";
+        #     width = 380;
+        #     height = 200;
+        #     origin = "top-right";
+        #     offset = "5x5";
+        #     scale = 0;
+        #     notification_limit = 20;
 
-          progress_bar = true;
-          progress_bar_height = 9;
-          progress_bar_frame_width = 1;
-          progress_bar_min_width = 150;
-          progress_bar_max_width = 400;
-          progress_bar_corner_radius = 0;
-          icon_corner_radius = 0;
-          indicate_hidden = true;
-          transparency = 0;
-          separator_height = 3;
-          padding = 8;
-          horizontal_padding = 8;
-          text_icon_padding = 0;
-          frame_width = 1;
-          frame_color = "#${config.lib.stylix.colors.base00}";
-          gap_size = 5;
-          separator_color = "frame";
-          sort = true;
+        #     progress_bar = true;
+        #     progress_bar_height = 9;
+        #     progress_bar_frame_width = 1;
+        #     progress_bar_min_width = 150;
+        #     progress_bar_max_width = 400;
+        #     progress_bar_corner_radius = 0;
+        #     icon_corner_radius = 0;
+        #     indicate_hidden = true;
+        #     transparency = 0;
+        #     separator_height = 3;
+        #     padding = 8;
+        #     horizontal_padding = 8;
+        #     text_icon_padding = 0;
+        #     frame_width = 1;
+        #     frame_color = "#${config.lib.stylix.colors.base00}";
+        #     gap_size = 5;
+        #     separator_color = "frame";
+        #     sort = true;
 
-          font = "${config.stylix.fonts.sansSerif.name} ${toString config.stylix.fonts.sizes.applications}";
-          markup = "full";
-          format = "<b><span foreground='#${config.home-manager.theme.accent}'>%s</span></b>\n<span foreground='#${config.lib.stylix.colors.base05}'>%b</span>";
-          alignment = "left";
-          vertical_alignment = "center";
-          show_age_threshold = 60;
-          ellipsize = "middle";
-          ignore_newline = false;
-          stack_duplicates = true;
-          hide_duplicate_count = true;
-          show_indicators = false;
+        #     font = "${config.stylix.fonts.sansSerif.name} ${toString config.stylix.fonts.sizes.applications}";
+        #     markup = "full";
+        #     format = "<b><span foreground='#${config.home-manager.theme.accent}'>%s</span></b>\n<span foreground='#${config.lib.stylix.colors.base05}'>%b</span>";
+        #     alignment = "left";
+        #     vertical_alignment = "center";
+        #     show_age_threshold = 60;
+        #     ellipsize = "middle";
+        #     ignore_newline = false;
+        #     stack_duplicates = true;
+        #     hide_duplicate_count = true;
+        #     show_indicators = false;
 
-          enable_recursive_icon_lookup = true;
-          icon_theme = "Papirus-Dark";
-          icon_position = "left";
-          min_icon_size = 64;
-          max_icon_size = 32;
+        #     enable_recursive_icon_lookup = true;
+        #     # icon_theme = "Papirus-Dark";
+        #     # icon_position = "left";
+        #     min_icon_size = 64;
+        #     max_icon_size = 32;
 
-          sticky_history = true;
-          history_length = 20;
+        #     sticky_history = true;
+        #     history_length = 20;
 
-          browser = "${pkgs.xdg-utils}/bin/xdg-open";
-          always_run_script = true;
-          title = "Dunst";
-          class = "Dunst";
-          corner_radius = 0;
-          ignore_dbusclose = false;
-          force_xwayland = false;
-          force_xinerama = false;
+        #     browser = "${pkgs.xdg-utils}/bin/xdg-open";
+        #     always_run_script = true;
+        #     title = "Dunst";
+        #     class = "Dunst";
+        #     corner_radius = 0;
+        #     ignore_dbusclose = false;
+        #     force_xwayland = false;
+        #     force_xinerama = false;
 
-          mouse_left_click = "close_current";
-          mouse_middle_click = "do_action, close_current";
-          mouse_right_click = "close_all";
+        #     mouse_left_click = "close_current";
+        #     mouse_middle_click = "do_action, close_current";
+        #     mouse_right_click = "close_all";
 
-          highlight = "#${config.home-manager.theme.accent}";
-          background = "#${config.lib.stylix.colors.base00}";
-          foreground = "#${config.lib.stylix.colors.base05}";
-        };
+        #     highlight = "#${config.home-manager.theme.accent}";
+        #     background = "#${config.lib.stylix.colors.base00}";
+        #     foreground = "#${config.lib.stylix.colors.base05}";
+        #   };
 
         experimental = {
           per_monitor_dpi = false;
