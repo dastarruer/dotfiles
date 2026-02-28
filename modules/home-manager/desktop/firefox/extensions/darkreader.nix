5578575f6d098b448f3906e820f19402a74622f4
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-02-28 12:54:22 -0400

    remove unnecessary quotes from darkreader settings

--- modules/home-manager/desktop/firefox/extensions/darkreader.nix
+++ modules/home-manager/desktop/firefox/extensions/darkreader.nix
@@ -12,36 +12,36 @@
       force = true;
 
       settings = {
-        "schemeVersion" = 2;
-        "enabled" = true;
-        "fetchNews" = true;
-        "theme" = {
-          "mode" = 1;
-          "brightness" = 100;
-          "contrast" = 100;
-          "grayscale" = 0;
-          "sepia" = 0;
-          "useFont" = true;
-          fontFamily = "${config.stylix.fonts.sansSerif.name}";
-          "textStroke" = 0;
-          "engine" = "dynamicTheme";
-          "stylesheet" = "";
-          "darkSchemeBackgroundColor" = "#${config.lib.stylix.colors.base00}";
-          "darkSchemeTextColor" = "#${config.lib.stylix.colors.base05}";
-          "lightSchemeBackgroundColor" = "#${config.lib.stylix.colors.base05}";
-          "lightSchemeTextColor" = "#${config.lib.stylix.colors.base00}";
-          "scrollbarColor" = "auto";
-          "selectionColor" = "auto";
-          "styleSystemControls" = false;
-          "lightColorScheme" = "Default";
-          "darkColorScheme" = "Default";
-          "immediateModify" = false;
+        schemeVersion = 2;
+        enabled = true;
+        fetchNews = true;
+        theme = {
+          mode = 1;
+          brightness = 100;
+          contrast = 100;
+          grayscale = 0;
+          sepia = 0;
+          useFont = true;
+          ontFamily = "${config.stylix.fonts.sansSerif.name}";
+          textStroke = 0;
+          engine = "dynamicTheme";
+          stylesheet = "";
+          darkSchemeBackgroundColor = "#${config.lib.stylix.colors.base00}";
+          darkSchemeTextColor = "#${config.lib.stylix.colors.base05}";
+          lightSchemeBackgroundColor = "#${config.lib.stylix.colors.base05}";
+          lightSchemeTextColor = "#${config.lib.stylix.colors.base00}";
+          scrollbarColor = "auto";
+          selectionColor = "auto";
+          styleSystemControls = false;
+          lightColorScheme = "Default";
+          darkColorScheme = "Default";
+          immediateModify = false;
         };
-        "presets" = [];
-        "customThemes" = [];
-        "enabledByDefault" = true;
-        "enabledFor" = [];
-        "disabledFor" = [
+        presets = [];
+        customThemes = [];
+        enabledByDefault = true;
+        enabledFor = [];
+        disabledFor = [
           "coolors.co"
           "learning.amplify.com"
           "student.amplify.com"
@@ -73,28 +73,28 @@
           "mail.google.com"
           "github.com"
         ];
-        "changeBrowserTheme" = false;
-        "syncSettings" = false;
-        "syncSitesFixes" = true;
-        "automation" = {
-          "enabled" = false;
-          "mode" = "system";
-          "behavior" = "OnOff";
+        changeBrowserTheme = false;
+        syncSettings = false;
+        syncSitesFixes = true;
+        automation = {
+          enabled = false;
+          mode = "system";
+          behavior = "OnOff";
         };
-        "time" = {
-          "activation" = "18:00";
-          "deactivation" = "9:00";
+        time = {
+          activation = "18:00";
+          deactivation = "9:00";
         };
-        "location" = {
-          "latitude" = null;
-          "longitude" = null;
+        location = {
+          latitude = null;
+          longitude = null;
         };
-        "previewNewDesign" = true;
-        "enableForPDF" = true;
-        "enableForProtectedPages" = true;
-        "enableContextMenus" = false;
-        "detectDarkTheme" = false;
-        "displayedNews" = [
+        previewNewDesign = true;
+        enableForPDF = true;
+        enableForProtectedPages = true;
+        enableContextMenus = false;
+        detectDarkTheme = false;
+        displayedNews = [
           "thanks-2023"
         ];
       };
