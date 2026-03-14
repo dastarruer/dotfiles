38f9f89b8351f8d1a2fccbaf18a6bf0cf96f2c21
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-03-14 11:59:30 -0400

    backup obsidian vault to gdrive

--- modules/home-manager/desktop/obsidian.nix
+++ modules/home-manager/desktop/obsidian.nix
@@ -29,8 +29,6 @@ in {
       ];
     };
 
-    # my notes are NOT going on the cloud. god know what they'll find in there...
-    services.restic.backups.drive.exclude = ["${config.home.homeDirectory}/Documents/vault"];
     home-manager.services.backup.backupPaths = lib.mkIf backup.enable ["${config.home.homeDirectory}/Documents/vault"];
 
     wayland.windowManager.hyprland.settings = lib.mkIf hyprland.enable {
