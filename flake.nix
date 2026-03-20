d36d36b140a89674c95477e767d43dfe339ca4cc
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-03-20 11:13:59 -0400

    Add repo specific ide settings

--- flake.nix
+++ flake.nix
@@ -81,7 +81,15 @@
     ...
   }: let
     system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
   in {
+    devShells.${system}.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+        nixd
+        alejandra
+      ];
+    };
+
     nixosConfigurations.dastarruer = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs system;};
       modules = [
