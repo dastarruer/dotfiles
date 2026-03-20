3358a81d7b3638ddf018165b8e1c728d2c455bcc
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-03-20 11:51:53 -0400

    add backup for freelancer variations

--- modules/home-manager/desktop/gaming/steam/games/hitman-woa.nix
+++ modules/home-manager/desktop/gaming/steam/games/hitman-woa.nix
@@ -17,5 +17,11 @@ in {
   # Backup hitman mods
   home-manager.services.backup.backupPaths = lib.mkIf backup.enable [
     "${config.home.homeDirectory}/.local/share/Steam/steamapps/common/HITMAN\ 3/Simple\ Mod\ Framework"
+    "${config.home.homeDirectory}/.local/share/Steam/steamapps/common/HITMAN\ 3/README.md"
+
+    # Freelancer variations stuff
+    "${config.home.homeDirectory}/.local/share/Steam/steamapps/common/HITMAN\ 3/Retail/mods/MissionCompanion.dll"
+    "${config.home.homeDirectory}/.local/share/Steam/steamapps/common/HITMAN\ 3/Retail/mods/FreelancerVariations.json"
+    "${config.home.homeDirectory}/.local/share/Steam/steamapps/common/HITMAN\ 3/Retail/mods/missioncompanion.ini"
   ];
 }
