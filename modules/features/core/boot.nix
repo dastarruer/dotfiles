a6ba4225ad82301ba53a3de486a796047e3a3276
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-04-07 11:53:56 -0400

    move kernel systctl params to boot

--- modules/features/core/boot.nix
+++ modules/features/core/boot.nix
@@ -86,5 +86,19 @@
       consoleLogLevel = 0;
       initrd.verbose = false;
     };
+
+    kernel.sysctl = {
+      # Only use swap when completely necessary
+      "vm.swappiness" = 20;
+
+      # For RAM intensive applications that may not use all the ram it allocates, promise more ram than is available (only up to a certain point. Otherwise, the process may be killed by earlyoom).
+      # https://medium.com/@charles.vissol/optimize-your-linux-69c70320d852
+      # "vm.overcommit-memory" = 2;
+      "vm.overcommit-memory" = 0;
+
+      # write dirty pages to disk once 5% of ram holds dirty pages
+      # https://medium.com/@charles.vissol/optimize-your-linux-69c70320d852
+      "vm.dirty_background_ratio" = 5;
+    };
   };
 }
