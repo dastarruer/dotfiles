df0e6a0bbe4e6f572243e9cdc0482b864b5e99dd
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-04-07 12:38:03 -0400

    convert scripts to dendritic

---
+++ modules/features/wm/power.nix
@@ -0,0 +1,66 @@
+{inputs, ...}: {
+  flake.nixosModules.wm = {
+    config,
+    pkgs,
+    lib,
+    ...
+  }: let
+    hyprland = config.wayland.windowManager.hyprland;
+    locker = config.home-manager.window-manager.screen-locker;
+    rofi = config.home-manager.window-manager.rofi;
+
+    lockCmd =
+      if locker == "hyprlock"
+      then "${lib.getExe pkgs.hyprlock}"
+      else if locker == "swaylock"
+      then "${lib.getExe pkgs.swaylock}"
+      else "true";
+
+    logoutCmd = lib.optionalString hyprland.enable "${pkgs.hyprland}/bin/hyprctl dispatch exit";
+
+    launcherCmd =
+      if rofi.enable
+      then "${lib.getExe pkgs.rofi} -dmenu -i -p 'Power Menu:'"
+      else "cat";
+  in {
+    nixpkgs.overlays = [
+      (final: prev: {
+        power = prev.writeShellApplication {
+          name = "power";
+
+          runtimeInputs = with prev; [
+            systemd
+            coreutils
+          ];
+
+          text = ''
+            OPTIONS=" Lock\n󰗽 Logout\n󰥔 Suspend\n Reboot\n Shutdown"
+
+            CHOICE=$(echo -e "$OPTIONS" | ${launcherCmd})
+
+            case "$CHOICE" in
+              " Lock")
+                  ${lockCmd} & disown
+                  ;;
+              "󰗽 Logout")
+                  ${logoutCmd}
+                  ;;
+              "󰥔 Suspend")
+                  systemctl suspend
+                  ;;
+              " Reboot")
+                  systemctl reboot
+                  ;;
+              " Shutdown")
+                  systemctl poweroff
+                  ;;
+              *)
+                  exit 0
+                  ;;
+            esac
+          '';
+        };
+      })
+    ];
+  };
+}
