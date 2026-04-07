8ccc5ac44316db0f13ea52f7023185633721ea24
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-04-06 22:08:02 -0400

    convert steam link to dendritic

--- modules/features/desktop/steam-link.nix
+++ modules/features/desktop/steam-link.nix
@@ -1,21 +1,15 @@
-{
-  config,
-  lib,
-  ...
-}: {
-  options = {
-    home-manager.desktop.steam-link.enable = lib.mkOption {
-      type = lib.types.bool;
-      default = config.home-manager.desktop.enable;
-      description = "Enable Steam Link.";
-    };
-  };
+{self, ...}: {
+  flake.nixosModules.desktop_steam-link = {...}: {
+    imports = [
+      self.nixosModules.flatpak
+    ];
 
-  config = lib.mkIf config.home-manager.desktop.steam-link.enable {
-    services.flatpak = {
-      packages = [
-        "flathub:app/com.valvesoftware.SteamLink/x86_64/stable"
-      ];
+    home-manager.users.dastarruer = {
+      services.flatpak = {
+        packages = [
+          "flathub:app/com.valvesoftware.SteamLink/x86_64/stable"
+        ];
+      };
     };
   };
 }
