946155410f15988351953979ea6dbbed0e789d5e
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-04-06 22:04:22 -0400

    convert obsidian to dendritic

---
+++ modules/features/desktop/flatpak.nix
@@ -0,0 +1,47 @@
+# Does not need to be manually imported; will be imported as is necessary
+{...}: {
+  flake.nixosModules.flatpak = {
+    config,
+    pkgs,
+    lib,
+    ...
+  }: {
+    home-manager.users.dastarruer = let
+      hyprland = config.home-manager.window-manager.hyprland;
+    in {
+      # This is needed for launching flatpaks with rofi
+      home.packages = with pkgs; [
+        flatpak
+      ];
+
+      # FUTURE ME IF flatpak list throws an error, then run:
+      # sudo rm -rf /var/lib/flatpak/repo
+      # sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+      # sudo flatpak remote-add --if-not-exists flathub-beta https://dl.flathub.org/beta-repo/flathub-beta.flatpakrepo
+      services.flatpak = {
+        enable = true;
+
+        # Add remotes here
+        remotes = {
+          "flathub" = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+          "flathub-beta" = "https://dl.flathub.org/beta-repo/flathub-beta.flatpakrepo";
+        };
+
+        overrides = {
+          "global".Context = {
+            # Give all flatpaks access to the home dir
+            filesystems = [
+              "home"
+            ];
+
+            # Remove x11 support for flatpaks, making them run only on wayland
+            sockets = lib.mkIf hyprland.enable [
+              "!x11"
+              "!fallback-x11"
+            ];
+          };
+        };
+      };
+    };
+  };
+}
---
+++ modules/features/desktop/obsidian.nix
@@ -0,0 +1,38 @@
+{self, ...}: {
+  flake.nixosModules.desktop_obsidian = {
+    config,
+    lib,
+    ...
+  }: {
+    imports = [
+      self.nixosModules.flatpak
+    ];
+
+    home-manager.users.dastarruer = let
+      hyprland = config.programs.hyprland;
+      backup = config.services.restic;
+    in {
+      # Use the flatpak, which does not ask for login every reboot
+      services.flatpak = {
+        packages = [
+          "flathub:app/md.obsidian.Obsidian/x86_64/stable"
+        ];
+
+        # Enable wayland support for obsidian
+        overrides."md.obsidian.Obsidian".Context.sockets = lib.mkIf hyprland.enable [
+          "wayland"
+          "!x11"
+          "!fallback-x11"
+        ];
+      };
+
+      home-manager.services.backup.backupPaths = lib.mkIf backup.enable ["${config.home.homeDirectory}/Documents/vault"];
+
+      wayland.windowManager.hyprland.settings = lib.mkIf hyprland.enable {
+        windowrule = [
+          "workspace 10 silent, match:class obsidian"
+        ];
+      };
+    };
+  };
+}
