e4953885416ba0b13530ea6af8918fb2bd066808
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-04-13 16:42:52 -0400

    Add windowrule

--- modules/features/desktop/kdeconnect.nix
+++ modules/features/desktop/kdeconnect.nix
@@ -1,5 +1,12 @@
 {...}: {
-  flake.nixosModules.desktop_kdeconnect = {...}: {
+  flake.nixosModules.desktop_kdeconnect = {
+    config,
+    lib,
+    ...
+  }: let
+    hmConfig = config.home-manager.users.dastarruer;
+    hyprland = hmConfig.wayland.windowManager.hyprland;
+  in {
     # https://www.reddit.com/r/NixOS/comments/xz4m6m/how_to_use_kdeconnect_on_nixos/
     programs.kdeconnect = {
       enable = true;
@@ -17,6 +24,16 @@
           to = 1764;
         } # KDE Connect
       ];
+    };
+
+    home-manager.users.dastarruer = {
+      wayland.windowManager.hyprland.settings =
+        lib.mkIf hyprland.enable {
+          windowrule = [
+            # Float the 'Receiving file' popup that occurs when transferring a large file
+            "float on, match:title ^(Receiving file)(.*)$"
+          ];
+        };
     };
   };
 }
