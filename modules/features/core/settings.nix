4ab9b83bb5c9dc1fb76147e485756df8a5e0343a
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-04-25 11:08:29 -0400

    add backupCommand

--- modules/features/core/settings.nix
+++ modules/features/core/settings.nix
@@ -1,11 +1,22 @@
 {inputs, ...}: {
-  flake.nixosModules.core = {...}: {
+  flake.nixosModules.core = {
+    pkgs,
+    lib,
+    ...
+  }: {
     # Suppress warning (https://nixos.org/manual/nixos/stable/options.html#opt-system.stateVersion)
     system.stateVersion = "25.05";
 
     nixpkgs.config.allowUnfree = true;
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
+    home-manager = {
+      useGlobalPkgs = true;
+      useUserPackages = true;
+
+      # Delete any files that home-manager is too scared to overwrite
+      # It's ok home manager, you can do what you want. stop FAILING BECAUSE YOU CAN'T DELETE A GODDAMN FILE-
+      # 'o-oh i just don't want to delete anything i shouldnt-' THAT'S THE WHOLE POINT OF USING YOU DELETE IT PLEASE
+      backupCommand = "${lib.getExe pkgs.trash-cli}";
+    };
 
     nix = {
       # For nixd i think
