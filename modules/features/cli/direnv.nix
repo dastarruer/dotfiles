2460eff04e58f33b7801e245cd085231452c0f66
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-05-10 14:32:06 -0400

    add direnv setting for zed

--- modules/features/cli/direnv.nix
+++ modules/features/cli/direnv.nix
@@ -1,5 +1,7 @@
 {...}: {
-  flake.nixosModules.cli_direnv = {config, ...}: {
+  flake.nixosModules.cli_direnv = {config,lib, ...}: let
+    editor = config.custom.desktop.editor;
+  in {
     home-manager.users.dastarruer = {
       programs.direnv = {
         enable = true;
@@ -8,6 +10,9 @@
         # For nix flake integration
         nix-direnv.enable = true;
       };
+
+      # Load nix devshells when opening project w editor
+      programs.zed-editor.userSettings.load_direnv = lib.mkIf (editor == "zed") "shell_hook";
     };
   };
 }
