718408c641e622765dbf6b6bb5264e074c03aa07
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-05-21 18:59:53 -0400

    fix bluetooth kernel regression

--- modules/features/core/boot.nix
+++ modules/features/core/boot.nix
@@ -37,6 +37,17 @@
       # Get the latest kernel
       kernelPackages = pkgs.linuxPackages_latest;
 
+      # bluetooth no worky: https://github.com/NixOS/nixpkgs/issues/521528
+      kernelPatches = [
+        {
+          name = "Bluetooth: btmtk: accept too short WMT FUNC_CTRL events";
+          patch = pkgs.fetchurl {
+            url = "https://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git/patch/?id=162b1adeb057d28ad84fd8a03f3c50cf08db5c62";
+            hash = "sha256-ij0hQmC0U++AdXWQy6nycnDe6z4yaMoQIrSiLal5DHc=";
+          };
+        }
+      ];
+
       # Spalsh screen
       plymouth = {
         enable = true;
