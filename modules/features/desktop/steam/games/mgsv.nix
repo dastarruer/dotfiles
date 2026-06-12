b9d4a001824b84ad632840e79cbc1e2058ddda47
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-06-12 09:35:00 -0400

    Add mgsv config

---
+++ modules/features/desktop/steam/games/mgsv.nix
@@ -0,0 +1,11 @@
+# Metal Gear Solid V
+{...}: {
+  flake.nixosModules.desktop_steam = {...}: {
+    programs.steam.config = {
+      apps.mgsv = {
+        id = 287700;
+        compatTool = "proton_7"; # If using latest version then 'Failed to load Steam API' error pops up instead. This seems to run the game just fine
+      };
+    };
+  };
+}
