886e6b11ef2747f9105d86049073b557f7a0cbfd
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-08-15 16:02:32 -0400

    add gamescope to hitman

--- modules/features/desktop/steam/games/hitman-woa.nix
+++ modules/features/desktop/steam/games/hitman-woa.nix
@@ -108,6 +108,22 @@
           text = ''systemctl start --user peacock'';
         });
         wrappers = [
+          (lib.getExe config.programs.gamescope.package)
+          # set window to monitor resolution
+          "-W"
+          "1920"
+          "-H"
+          "1080"
+          # render game at 720p
+          "-w"
+          "1600"
+          "-h"
+          "900"
+          # upscale to monitor resolution
+          "-F"
+          "fsr"
+          "-f"
+          "--"
           (lib.getExe config.programs.gamemode.package)
         ];
         args = [
