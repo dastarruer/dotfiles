092109321979bf99803cb2a188f56a4b134fee5e
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-08-26 22:49:07 -0400

    disable conservation mode settings

--- modules/hosts/laptop/configuration.nix
+++ modules/hosts/laptop/configuration.nix
@@ -133,11 +133,7 @@
     };
 
     # Enable conservation mode
-    # REMOVE IF USING TLP
     boot.kernelModules = ["ideapad_laptop"];
-    systemd.tmpfiles.rules = [
-      "w /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode - - - - 1"
-    ];
 
     # Disable touchpad as mouse for dualshock connected via USB and Bluetooth (https://wiki.archlinux.org/title/Gamepad#Disable_touchpad_acting_as_mouse)
     services.udev.extraRules = ''
