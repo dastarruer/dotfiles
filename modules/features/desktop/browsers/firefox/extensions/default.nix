2e7a27685cf342e94acafea99cd5dff07d9325c5
Ayush Pramanik <ayushpramanik399@gmail.com>
2026-09-06 11:17:01 -0400

    pin extension windows

--- modules/features/desktop/browsers/firefox/extensions/default.nix
+++ modules/features/desktop/browsers/firefox/extensions/default.nix
@@ -8,6 +8,7 @@
     profile = config.custom.desktop.browser.profile;
     firefoxPkgs = inputs.firefox-addons.packages.${pkgs.stdenv.system};
     browser = config.custom.desktop.browser.kind;
+    hyprland = config.custom.wm.wm == "hyprland";
   in
     lib.mkIf (browser == "firefox") {
       home-manager.users.dastarruer = {
@@ -23,6 +24,40 @@
             bitwarden
           ];
         };
+
+        # Float browser extension windows: https://wiki.hypr.land/configuring/code-snippets/#float-browsers-extension-windows
+        # Only difference here is that floating windows are also pinned
+        wayland.windowManager.hyprland.extraLuaFiles."float_extension_windows".content = lib.mkIf hyprland ''
+          hl.on("window.open", function(w)
+              if w.class ~= "firefox" then return end
+              if w.initial_title ~= "Mozilla Firefox" then return end
+
+              local ff_windows = hl.get_windows({ class = "firefox" })
+              if #ff_windows <= 1 then return end
+
+              hl.dispatch(hl.dsp.window.float({ action = "set", window = w }))
+
+              local sub
+              sub = hl.on("window.title", function(tw)
+                  if tw.address ~= w.address then return end
+                  if tw.title == ""
+                      or tw.title == "Mozilla Firefox"
+                      or tw.title == "about:blank"
+                      or tw.title:match("^about:.*Mozilla Firefox$") then return end
+
+                  sub:remove()
+
+                  if tw.title:match("^Extension:") then
+                      hl.dispatch(hl.dsp.window.resize({ x = 800, y = 600, window = tw }))
+                      hl.dispatch(hl.dsp.window.center({ window = tw }))
+                      hl.dispatch(hl.dsp.window.pin({ window = tw }))
+                      hl.dispatch(hl.dsp.focus({ window = tw }))
+                  else
+                      hl.dispatch(hl.dsp.window.float({ action = "unset", window = tw }))
+                  end
+              end)
+          end)
+        '';
       };
     };
 }
